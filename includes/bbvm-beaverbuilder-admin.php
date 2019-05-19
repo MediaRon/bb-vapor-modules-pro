51a09463ac3f44e1302a536f4da460194562042d
Ronald Huereca <rhuereca@updraftplus.com>
2019-05-18 21:36:41 -0500

    Adding license information and submitting to EDD.

--- includes/bbvm-beaverbuilder-admin.php
+++ includes/bbvm-beaverbuilder-admin.php
@@ -116,6 +116,86 @@ class BBVapor_BeaverBuilder_Admin {
 			<div class="notice notice-success"><p><?php esc_html_e( 'Cache cleared!', 'bb-vapor-modules-pro' ); ?></p></div>
 			<?php
 		}
+		if ( isset( $_POST['submit'] ) && isset( $_POST['options'] ) ) {
+
+
+			// Check for valid license
+			$store_url = 'https://bbvapormodules.com';
+			$api_params = array(
+				'edd_action' => 'activate_license',
+				'license'    => $_POST['options']['license'],
+				'item_name'  => urlencode( 'BB Vapor Modules Pro' ),
+				'url'        => home_url()
+			);
+			// Call the custom API.
+			$response = wp_remote_post( $store_url, array( 'timeout' => 15, 'sslverify' => false, 'body' => $api_params ) );
+
+			// make sure the response came back okay
+			if ( is_wp_error( $response ) || 200 !== wp_remote_retrieve_response_code( $response ) ) {
+
+				if ( is_wp_error( $response ) ) {
+					$license_message = $response->get_error_message();
+				} else {
+					$license_message = __( 'An error occurred, please try again.', 'bb-vapor-modules-pro' );
+				}
+
+			} else {
+
+				$license_data = json_decode( wp_remote_retrieve_body( $response ) );
+
+				if ( false === $license_data->success ) {
+					delete_site_option( 'bbvm_for_beaver_builder_license_status' );
+					switch( $license_data->error ) {
+
+						case 'expired' :
+
+							$license_message = sprintf(
+								__( 'Your license key expired on %s.', 'bb-vapor-modules-pro' ),
+								date_i18n( get_option( 'date_format' ), strtotime( $license_data->expires, current_time( 'timestamp' ) ) )
+							);
+							break;
+
+						case 'disabled' :
+						case 'revoked' :
+
+							$license_message = __( 'Your license key has been disabled.', 'bb-vapor-modules-pro' );
+							break;
+
+						case 'missing' :
+
+							$license_message = __( 'Invalid license.', 'bb-vapor-modules-pro' );
+							break;
+
+						case 'invalid' :
+						case 'site_inactive' :
+
+							$license_message = __( 'Your license is not active for this URL.', 'bb-vapor-modules-pro' );
+							break;
+
+						case 'item_name_mismatch' :
+
+							$license_message = sprintf( __( 'This appears to be an invalid license key for %s.', 'bb-vapor-modules-pro' ), 'Simple Comment Editing Options' );
+							break;
+
+						case 'no_activations_left':
+
+							$license_message = __( 'Your license key has reached its activation limit.', 'bb-vapor-modules-pro' );
+							break;
+
+						default :
+
+							$license_message = __( 'An error occurred, please try again.', 'bb-vapor-modules-pro' );
+							break;
+					}
+				}
+				if ( empty( $license_message ) ) {
+					update_site_option( 'bbvm_for_beaver_builder_license_status', $license_data->license );
+					update_site_option( 'bbvm_for_beaver_builder_license', sanitize_text_field( $_POST['options']['license'] ) );
+				}
+			}
+		}
+		$license_status = get_site_option( 'bbvm_for_beaver_builder_license_status', false );
+		$license = get_site_option( 'bbvm_for_beaver_builder_license', '' );
 		?>
 		<div class="wrap">
 			<form action="<?php echo esc_url( add_query_arg( array( 'page' => 'bb-vapor-modules-pro' ), admin_url( 'options-general.php' ) ) ); ?>" method="POST">
@@ -123,6 +203,24 @@ class BBVapor_BeaverBuilder_Admin {
 				<h2><?php esc_html_e( 'Vapor Modules for Beaver Builder', 'breadcrumbs-for-beaver-builder' ); ?></h2>
 				<table class="form-table">
 					<tbody>
+					<tr>
+							<th scope="row"><label for="bbvm-license"><?php esc_html_e( 'Enter Your License', 'bb-vapor-modules-pro' ); ?></label></th>
+							<td>
+								<input id="bbvm-license" class="regular-text" type="text" value="<?php echo esc_attr( $license ); ?>" name="options[license]" /><br />
+								<?php
+								if( false === $license || empty( $license ) ) {
+									printf('<p>%s</p>', esc_html__( 'Please enter your licence key.', 'bb-vapor-modules-pro' ) );
+								} else {
+									printf('<p>%s</p>', esc_html__( 'Your license is valid and you will now receive update notifications.', 'bb-vapor-modules-pro' ) );
+								}
+								?>
+								<?php
+								if( ! empty( $license_message ) ) {
+									printf( '<div class="updated error"><p><strong>%s</p></strong></div>', esc_html( $license_message ) );
+								}
+								?>
+							</td>
+						</tr>
 						<tr>
 							<th scope="row"><label for="bbvm-instagram"><?php esc_html_e( 'Connect to Instagram', 'bb-vapor-modules-pro' ); ?></label>
 							</th>
@@ -163,15 +261,9 @@ class BBVapor_BeaverBuilder_Admin {
 							?>
 							</td>
 						</tr>
-						<tr>
-							<th scope="row"><label for="bbvm-pro"><?php esc_html_e( 'Upgrade', 'bb-vapor-modules-pro' ); ?></label>
-							</th>
-							<td>
-								<a class="button button-secondary" href="https://bbvapormodules.com" target="_blank"><?php esc_html_e( 'Upgrade to Pro for more modules', 'bb-vapor-modules-pro' ); ?></a>
-							</td>
-						</tr>
 					</tbody>
 				</table>
+				<?php submit_button( __( 'Save Options', 'bb-vapor-modules-pro' ) ); ?>
 			</form>
 		</div>
 		<?php
