bc31d77f36f39b13179ffd47eaefc4f7c2ed885a
Ronald Huereca <ronald@mediaron.com>
2019-05-27 13:43:47 -0500

    Fixing post select term.

--- bbvm-modules/featured-category/includes/frontend.js.php
+++ bbvm-modules/featured-category/includes/frontend.js.php
@@ -1,33 +1,9 @@
-jQuery( 'body' ).on('change', '.bbvm-post-select:visible, .bbvm-taxonomy-select:visible', { tax: '<?php echo esc_js( $settings->taxonomy_select ); ?>', term: <?php echo esc_js( $settings->terms_select ); ?>}, function( e ) {
+jQuery( 'body' ).on('change', '.bbvm-post-select:visible, .bbvm-taxonomy-select:visible', { tax: '<?php echo esc_js( isset( $settings->taxonomy_select ) ? $settings->taxonomy_select : 0 ); ?>', term: '<?php echo esc_js( isset( $settings->terms_select ) ? $settings->terms_select : 0 ); ?>'}, function( e ) {
 		if ( jQuery( '.fl-lightbox-content .fl-builder-settings' ).data('node') !== '<?php echo $id; ?>' ) {
 			return;
 		}
-		var bbvm_tax = e.data.tax;
-		var bbvm_term = e.data.term;
-		if ( jQuery( '.bbvm-post-select' ).val() != '0' ) {
-			if( undefined == jQuery( '.bbvm-taxonomy-select:visible :selected' ).val() ) {
-				var taxonomy = bbvm_tax;
-			} else {
-				if ( bbvm_tax == 0 ) {
-					var taxonomy = jQuery( '.bbvm-taxonomy-select:visible :selected' ).val();
-				} else {
-					var taxonomy = bbvm_tax;
-				}
-
-			}
-			if( undefined == jQuery( '.bbvm-term-select:visible :selected' ).val() ) {
-				var term = bbvm_term;
-			} else {
-				if ( bbvm_term == 0 ) {
-					var term = jQuery( '.bbvm-term-select:visible :selected' ).val();
-				} else {
-					var term = bbvm_term;
-				}
-			}
-		} else {
-			var taxonomy = 0;
-			var term = 0;
-		}
+		var taxonomy = e.data.tax;
+		var term = e.data.term;
 
 		jQuery.post( bbvm_beaver_builder_ajax_url, { action: 'bbvm_featured_bb_get_data', post_type: jQuery( '.bbvm-post-select:visible :selected' ).val(), taxonomy: taxonomy, term: term }, function( response ) {
 		jQuery( '.bbvm-taxonomy-select:first' ).html( response.taxonomies );
--- bbvm-modules/postselect/includes/frontend.js.php
+++ bbvm-modules/postselect/includes/frontend.js.php
@@ -1,35 +1,43 @@
-var bbvm_post_tax_<?php echo absint( $id ); ?> = "<?php echo esc_js( $settings->taxonomy_select ); ?>";
-var bbvm_post_term_<?php echo absint( $id ); ?> = "<?php echo esc_js( $settings->terms_select ); ?>";
+jQuery( 'body' ).on('change', '.bbvm-post-select:visible, .bbvm-taxonomy-select:visible', { tax: '<?php echo esc_js( isset( $settings->taxonomy_select ) ? $settings->taxonomy_select : '' ); ?>', term: '<?php echo esc_js( isset( $settings->terms_select ) ? $settings->terms_select : 0 ); ?>'}, function( e ) {
+		if ( jQuery( '.fl-lightbox-content .fl-builder-settings' ).data('node') !== '<?php echo $id; ?>' ) {
+			return;
+		}
 
-jQuery( 'body' ).unbind( "change" );
-jQuery( 'body' ).on('change', '.bbvm-post-select, .bbvm-taxonomy-select', function( e ) {
+		var bbvm_tax = e.data.tax;
+		var bbvm_term = e.data.term;
 
 		if ( jQuery( '.bbvm-post-select :selected' ).val() != '0' ) {
 			if( 'undefined' === jQuery( '.bbvm-taxonomy-select :selected' ).val() ) {
-				var taxonomy = bbvm_post_tax_<?php echo absint( $id ); ?>;
+				var taxonomy = bbvm_tax;
 			} else {
 				var taxonomy = jQuery( '.bbvm-taxonomy-select :selected' ).val();
+				if ( undefined == taxonomy ) {
+					taxonomy = bbvm_tax;
+				}
 			}
 			if( undefined === jQuery( '.bbvm-term-select :selected' ).val() ) {
-				var term = bbvm_post_term_<?php echo absint( $id ); ?>;
+				var term = bbvm_term;
 			} else {
 				var term = jQuery( '.bbvm-term-select :selected' ).val();
+				if ( undefined == term ) {
+					term = bbvm_term;
+				}
 			}
 		} else {
-			var taxonomy = 0;
-			var term = 0;
+			var taxonomy = e.data.tax;
+			var term = e.data.term;
 		}
-		jQuery.post( bbvm_beaver_builder_ajax_url, { action: 'bbvm_bb_get_data', post_type: jQuery( '.bbvm-post-select :selected' ).val(), taxonomy: taxonomy, term: term }, function( response ) {
-		jQuery( '.bbvm-taxonomy-select' ).html( response.taxonomies );
-		jQuery( '.bbvm-taxonomy-select option' ).each( function() {
+		jQuery.post( bbvm_beaver_builder_ajax_url, { action: 'bbvm_bb_get_data', post_type: jQuery( '.bbvm-post-select:visible :selected' ).val(), taxonomy: taxonomy, term: term }, function( response ) {
+		jQuery( '.bbvm-taxonomy-select:visible' ).html( response.taxonomies );
+		jQuery( '.bbvm-taxonomy-select:visible option' ).each( function() {
 			if( $(this).val() == response.taxonomy ) {
 				$( this ).attr( 'selected', 'selected' );
 				return;
 			}
 		} );
-		jQuery.post( bbvm_beaver_builder_ajax_url, { action: 'bbvm_bb_get_terms', post_type: jQuery( '.bbvm-post-select :selected' ).val(), taxonomy: response.taxonomy }, function( response ) {
-			jQuery( '.bbvm-term-select' ).html( response );
-			jQuery( '.bbvm-term-select option' ).each( function() {
+		jQuery.post( bbvm_beaver_builder_ajax_url, { action: 'bbvm_bb_get_terms', post_type: jQuery( '.bbvm-post-select:visible :selected' ).val(), taxonomy: response.taxonomy }, function( response ) {
+			jQuery( '.bbvm-term-select:visible' ).html( response );
+			jQuery( '.bbvm-term-select:visible option' ).each( function() {
 				if( $(this).val() == term ) {
 					$( this ).attr( 'selected', 'selected' );
 					return;
