0e549de115ed3f3c8d5d9527906f991f5037803e
Ronald Huereca <ronald@mediaron.com>
2019-05-28 19:11:27 -0500

    Version bump.

--- bbvm-modules/variable-headings/bbvm-variable-headings.php
+++ bbvm-modules/variable-headings/bbvm-variable-headings.php
@@ -15,19 +15,48 @@ class BBVapor_Variable_Headings_Module extends FLBuilderModule {
 		));
 	}
 }
-/**
- * Register the module and its form settings.
- */
+FLBuilder::register_settings_form(
+	'bbvm_variable_headlines', array(
+		'title' => __( 'Add Headline Text', 'bb-vapor-modules-pro' ),
+		'tabs'  => array(
+			'general'      => array(
+				'title'         => __('General', 'bb-vapor-modules-pro'),
+				'sections'      => array(
+					'general'       => array(
+						'title'         => __( 'Add Headline Text', 'bb-vapor-modules-pro' ),
+						'fields'        => array(
+							'headline' => array(
+								'type' => 'text',
+								'label' => __( 'Headline Text', 'bb-vapor-modules' ),
+								'default' => '',
+							),
+							'headline_color' => array(
+								'type' => 'color',
+								'label' => __( 'Headline Color', 'bb-vapor-modules' ),
+								'show_reset' => true,
+							),
+							'headline_typography' => array(
+								'type' => 'typography',
+								'label' => __( 'Headline Typography', 'bb-vapor-modules' ),
+								'responsive' => true,
+							)
+						)
+					)
+				),
+			),
+		)
+	)
+);
 FLBuilder::register_module('BBVapor_Variable_Headings_Module', array(
 	'general'       => array( // Tab
 		'title'         => __('General', 'bb-vapor-modules-pro'), // Tab title
 		'sections'      => array( // Tab Sections
 			'general'       => array( // Section
-				'title'         => __('Headlines', 'bb-vapor-modules-pro'), // Section Title
+				'title'         => __('Headings', 'bb-vapor-modules-pro'), // Section Title
 				'fields'        => array( // Section Fields
 					'headline_tag' => array(
 						'type' => 'select',
-						'label' => __( 'Headline Tag', 'bb-vapor-modules-pro' ),
+						'label' => __( 'Heading Tag', 'bb-vapor-modules-pro' ),
 						'options' => array(
 							'h1' => 'H1',
 							'h2' => 'H2',
@@ -37,6 +66,13 @@ FLBuilder::register_module('BBVapor_Variable_Headings_Module', array(
 							'h6' => 'H6',
 						),
 						'default' => 'h2'
+					),
+					'headlines' => array(
+						'type'          => 'form',
+						'form'          => 'bbvm_variable_headlines',
+						'label'         => __( 'Heading', 'bb-vapor-modules-pro' ),
+						'multiple'      => true,
+						'preview_text'  => 'headline',
 					),
 					'headline_padding' => array(
 						'type' => 'dimension',
--- bbvm-modules/variable-headings/includes/frontend.php
+++ bbvm-modules/variable-headings/includes/frontend.php
@@ -1,4 +1,13 @@
-<div class="fl-bbvm-headlines-for-beaverbuilder">
-	<<?php echo $settings->headline_tag;?>
-	<?php echo 'none' !== $settings->animation_type ? 'class="animated infinite ' . esc_attr( $settings->animation_type ) . '"' : ''; ?>><?php echo esc_html( $settings->headline_text ); ?></<?php echo $settings->headline_tag; ?>>
+<div class="fl-bbvm-variable-headings-for-beaverbuilder">
+	<?php
+	$bbvm_count = 0;
+	echo sprintf( '<%s>', $settings->headline_tag );
+	foreach( $settings->headlines as $headline ) {
+		printf( '<span class="bbvm-variable-headline-%d">', $bbvm_count );
+		echo esc_html( $headline->headline ) . ' ';
+		echo '</span>';
+		$bbvm_count += 1;
+	}
+	echo sprintf( '</%s>', $settings->headline_tag );
+	?>
 </div>
