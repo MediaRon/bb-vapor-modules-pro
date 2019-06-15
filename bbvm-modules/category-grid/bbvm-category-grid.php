13c12c5a1662a2df27eaff77068a1f920094b403
Ronald Huereca <ronald@mediaron.com>
2019-06-14 22:08:32 -0500

    Allowing users the choice for where their categories come from.

--- bbvm-modules/category-grid/bbvm-category-grid.php
+++ bbvm-modules/category-grid/bbvm-category-grid.php
@@ -24,205 +24,239 @@ FLBuilder::register_module(
 			'title' => __( 'Category Selection', 'fl-builder' ),
 			'file'  => plugin_dir_path( __FILE__ ) . 'includes/loop-settings.php',
 		),
-		'general'       => array( // Tab
-			'title'         => __( 'General', 'bb-vapor-modules-pro' ), // Tab title
-			'sections'      => array( // Tab Sections
-				'general'       => array( // Section
-					'title'         => __('Category Selection', 'bb-vapor-modules-pro'), // Section Title
-					'fields'        => array( // Section Fields
-						'category_title' => array(
-							'type' => 'text',
-							'label' => __( 'Category Name', 'bb-vapor-modules-pro' ),
-							'help' => __( 'Leave blank to use the default category name', 'bb-vapor-modules-pro' ),
-							'default' => ''
+		'options'  => array( // Tab
+			'title'    => __( 'Options', 'bb-vapor-modules-pro' ), // Tab title
+			'sections' => array( // Tab Sections
+				'image' => array( // Section
+					'title'     => __( 'Image Selection', 'bb-vapor-modules-pro' ), // Section Title
+					'fields' => array( // Section Fields
+						'image_type' => array(
+							'type'   => 'select',
+							'label'   => __( 'Where are your images coming from?', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'acf'     => __( 'Advanced Custom Fields', 'bb-vapor-modules-pro' ),
+								'custom'  => __( 'Category Meta', 'bb-vapor-modules-pro' ),
+								'gallery' => __( 'A Gallery of Images', 'bb-vapor-modules-pro' ),
+							),
+							'toggle'  => array(
+								'acf'     => array(
+									'fields' => array(
+										'acf_field',
+									),
+								),
+								'custom'  => array(
+									'fields' => array(
+										'meta_key',
+									),
+								),
+								'gallery' => array(
+									'fields' => array(
+										'category_gallery',
+									),
+								),
+							),
 						),
-						'category_width' => array(
-							'type' => 'unit',
-							'label' => __( 'Category Title Max Width', 'bb-vapor-modules-pro' ),
-							'help' => __( 'The maximum width of the category text. Default is 70%.', 'bb-vapor-modules-pro' ),
-							'default' => '70',
-							'responsive' => true,
-							'description' => '%',
-						),
-						'category_title_padding' => array(
-							'type' => 'dimension',
-							'label' => __( 'Category Title Padding', 'bb-vapor-modules-pro' ),
-							'responsive' => true,
-						),
-						'min_height' => array(
-							'type'          => 'unit',
-							'label'         => __( 'Item Height', 'bb-vapor-modules-pro' ),
-							'default'       => '400',
-							'responsive'    => true,
-							'description'   => 'px',
-						),
-						'inner_margin' => array(
-							'type' => 'dimension',
-							'label' => __( 'Inner Margin', 'bb-vapor-modules-pro' ),
+						'default'    => 'gallery',
+					),
+					'acf_field' => array(
+						'type'  => 'text',
+						'label' => __( 'Enter Your ACF Custom Field Name Here', 'bb-vapor-modules-pro' ),
+					),
+					'custom'    => array(
+						'type'  => 'text',
+						'label' => __( 'Enter Your Custom Category Meta Name Here', 'bb-vapor-modules-pro' ),
+					),
+					'gallery'   => array(
+						'type'        => 'gallery',
+						'label'       => __( 'Enter Your Gallery Images Here', 'bb-vapor-modules-pro' ),
+						'description' => __( 'Each category will be given a descending gallery image, and loop back if there are not enough images', 'bb-vapor-modules-pro' ),
+					),
+
+				),
+				'box'   => array( // Section
+					'title'  => __( 'Box Options', 'bb-vapor-modules-pro' ), // Section Title
+					'fields' => array( // Section Fields
+						'inner_margin'        => array(
+							'type'        => 'dimension',
+							'label'       => __( 'Inner Margin', 'bb-vapor-modules-pro' ),
 							'description' => __( 'Recommended margin is 20', 'bb-vapor-modules-pro' ),
-							'responsive' => true
+							'responsive'  => true,
 						),
-						'link_category' => array(
-							'type'          => 'select',
-							'label'         => __( 'Link Entire Container to Category', 'bb-vapor-modules-pro' ),
+						'link_category'       => array(
+							'type'    => 'select',
+							'label'   => __( 'Link Entire Container to Category', 'bb-vapor-modules-pro' ),
 							'options' => array(
 								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
-								'no' => __( 'No', 'bb-vapor-modules-pro' ),
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
 							),
 							'default' => 'no',
 						),
 						'category_text_color' => array(
-							'type' => 'color',
-							'label' => __( 'Category Color', 'bb-vapor-modules-pro' ),
-							'default' => '000000'
+							'type'    => 'color',
+							'label'   => __( 'Category Color', 'bb-vapor-modules-pro' ),
+							'default' => '000000',
 						),
 						'category_typography' => array(
-							'type' => 'typography',
-							'label' => __( 'Category Typography', 'bb-vapor-modules-pro' ),
-							'responsive' => true
+							'type'       => 'typography',
+							'label'      => __( 'Category Typography', 'bb-vapor-modules-pro' ),
+							'responsive' => true,
 						),
 						'border_style' => array(
-							'type'          => 'select',
-							'label'         => __( 'Select Style', 'bb-vapor-modules-pro' ),
+							'type'    => 'select',
+							'label'   => __( 'Select Style', 'bb-vapor-modules-pro' ),
 							'options' => array(
 								'inner_border' => __( 'Inner Border', 'bb-vapor-modules-pro' ),
-								'no_border' => __( 'No Border', 'bb-vapor-modules-pro' ),
+								'no_border'    => __( 'No Border', 'bb-vapor-modules-pro' ),
 							),
 							'default' => 'no_border',
-							'toggle' => array( 'inner_border' => array( 'fields' => array( 'border_width', 'border_color', 'border_color_hover' ) ) )
+							'toggle'  => array(
+								'inner_border' => array(
+									'fields' => array(
+										'border_width',
+										'border_color',
+										'border_color_hover',
+									),
+								),
+							),
 						),
-						'border_width' => array(
-							'type' => 'unit',
-							'label' => __( 'Border Width', 'bb-vapor-modules-pro' ),
+						'border_width'        => array(
+							'type'        => 'unit',
+							'label'       => __( 'Border Width', 'bb-vapor-modules-pro' ),
 							'description' => 'px',
-							'default' => '2'
+							'default'     => '2',
 						),
-						'border_color' => array(
-							'type'          => 'color',
-							'label'         => __( 'Border Color', 'bb-vapor-modules-pro' ),
-							'default'       => '000000',
+						'border_color'        => array(
+							'type'    => 'color',
+							'label'   => __( 'Border Color', 'bb-vapor-modules-pro' ),
+							'default' => '000000',
 						),
-						'border_color_hover' => array(
-							'type'          => 'color',
-							'label'         => __( 'Border Hover Color', 'bb-vapor-modules-pro' ),
-							'default'       => '333333',
+						'border_color_hover'  => array(
+							'type'    => 'color',
+							'label'   => __( 'Border Hover Color', 'bb-vapor-modules-pro' ),
+							'default' => '333333',
 						),
-						'category_padding' => array(
-							'type'          => 'dimension',
-							'label'         => __( 'Category Padding', 'bb-vapor-modules-pro' ),
-							'responsive'    => true,
+						'category_padding'    => array(
+							'type'       => 'dimension',
+							'label'      => __( 'Category Padding', 'bb-vapor-modules-pro' ),
+							'responsive' => true,
 						),
-						'show_button' => array(
-							'type' => 'select',
-							'label' => __( 'Show Button', 'bb-vapor-modules-pro' ),
+						'show_button'         => array(
+							'type'    => 'select',
+							'label'   => __( 'Show Button', 'bb-vapor-modules-pro' ),
 							'options' => array(
 								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
-								'no' => __( 'No', 'bb-vapor-modules-pro' )
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
 							),
 							'default' => 'no',
-							'toggle' => array(
-								'yes' => array( 'tabs' => array( 'button' ) )
-							)
-						)
-					)
-				)
-			)
-		),
-		'photo'       => array( // Tab
-			'title'         => __('Background Photo', 'bb-vapor-modules-pro'), // Tab title
-			'sections'      => array( // Tab Sections
-				'photo'       => array( // Section
-					'title'         => __('Background Photo', 'bb-vapor-modules-pro'), // Section Title
-					'fields'        => array( // Section Fields
-						'background_photo' => array(
-							'type' => 'photo',
-							'label' => __( 'Background Photo', 'bb-vapor-modules-pro' ),
-							'show_remove'   => false,
+							'toggle'  => array(
+								'yes' => array(
+									'tabs' => array(
+										'button',
+									),
+								),
+							),
 						),
-						'background_overlay' => array(
-							'type'          => 'color',
-							'label'         => __( 'Background Overlay', 'bb-vapor-modules-pro' ),
-							'default'       => 'rgba(0,0,0,0.5)',
+					),
+				),
+			),
+		),
+		'overlay'  => array( // Tab
+			'title'    => __( 'Overlay', 'bb-vapor-modules-pro' ), // Tab title
+			'sections' => array( // Tab Sections
+				'photo' => array( // Section
+					'title'  => __( 'Overlay Options', 'bb-vapor-modules-pro' ), // Section Title
+					'fields' => array( // Section Fields
+						'background_overlay'       => array(
+							'type'       => 'color',
+							'label'      => __( 'Background Overlay', 'bb-vapor-modules-pro' ),
+							'default'    => 'rgba(0,0,0,0.5)',
 							'show_reset' => true,
 							'show_alpha' => true,
 
 						),
 						'background_overlay_hover' => array(
-							'type'          => 'color',
-							'label'         => __( 'Background Overlay Hover', 'bb-vapor-modules-pro' ),
-							'default'       => 'rgba(0,0,0,0.3)',
+							'type'       => 'color',
+							'label'      => __( 'Background Overlay Hover', 'bb-vapor-modules-pro' ),
+							'default'    => 'rgba(0,0,0,0.3)',
 							'show_reset' => true,
 							'show_alpha' => true,
 
 						),
-					)
-				)
-			)
+					),
+				),
+			),
 		),
-		'button'       => array( // Tab
-			'title'         => __('Button', 'bb-vapor-modules-pro'), // Tab title
-			'sections'      => array( // Tab Sections
-				'button'       => array( // Section
-					'title'         => __('Button', 'bb-vapor-modules-pro'), // Section Title
-					'fields'        => array( // Section Fields
-						'button_text' => array(
-							'type' => 'text',
-							'label' => __( 'Button Text', 'bb-vapor-modules-pro' ),
+		'button'   => array( // Tab
+			'title'    => __( 'Button', 'bb-vapor-modules-pro' ), // Tab title
+			'sections' => array( // Tab Sections
+				'button' => array( // Section
+					'title'  => __( 'Button', 'bb-vapor-modules-pro' ), // Section Title
+					'fields' => array( // Section Fields
+						'button_text'               => array(
+							'type'    => 'text',
+							'label'   => __( 'Button Text', 'bb-vapor-modules-pro' ),
 							'default' => __( 'View', 'bb-vapor-modules-pro' ),
 						),
-						'button_background' => array(
-							'type' => 'color',
-							'label' => __( 'Button Background', 'bb-vapor-modules-pro' ),
+						'button_background'         => array(
+							'type'       => 'color',
+							'label'      => __( 'Button Background', 'bb-vapor-modules-pro' ),
 							'show_reset' => true,
 							'show_alpha' => true,
-							'default' => 'rgba(0,0,0,0)'
+							'default'    => 'rgba(0,0,0,0)',
 						),
-						'button_background_hover' => array(
-							'type' => 'color',
-							'label' => __( 'Button Background Hover', 'bb-vapor-modules-pro' ),
+						'button_background_hover'   => array(
+							'type'       => 'color',
+							'label'      => __( 'Button Background Hover', 'bb-vapor-modules-pro' ),
 							'show_reset' => true,
 							'show_alpha' => true,
-							'default' => 'rgba(255,255,255,1)'
+							'default'    => 'rgba(255,255,255,1)',
 						),
-						'button_color' => array(
-							'type' => 'color',
-							'label' => __( 'Button Text Color', 'bb-vapor-modules-pro' ),
+						'button_color'              => array(
+							'type'       => 'color',
+							'label'      => __( 'Button Text Color', 'bb-vapor-modules-pro' ),
 							'show_reset' => true,
-							'default' => 'FFFFFF'
+							'default'    => 'FFFFFF',
+							'show_alpha' => true,
 						),
-						'button_color_hover' => array(
-							'type' => 'color',
-							'label' => __( 'Button Text Hover Color', 'bb-vapor-modules-pro' ),
+						'button_color_hover'        => array(
+							'type'       => 'color',
+							'label'      => __( 'Button Text Hover Color', 'bb-vapor-modules-pro' ),
 							'show_reset' => true,
-							'default' => '000000'
+							'show_alpha' => true,
+							'default'    => '000000',
 						),
-						'button_border_color' => array(
-							'type' => 'color',
-							'label' => __( 'Button Border Color', 'bb-vapor-modules-pro' ),
+						'button_border_color'       => array(
+							'type'       => 'color',
+							'label'      => __( 'Button Border Color', 'bb-vapor-modules-pro' ),
 							'show_reset' => true,
-							'default' => '000000'
+							'show_alpha' => true,
+							'default'    => '000000',
 						),
 						'button_border_color_hover' => array(
-							'type' => 'color',
-							'label' => __( 'Button Border Color on Hover', 'bb-vapor-modules-pro' ),
+							'type'       => 'color',
+							'label'      => __( 'Button Border Color on Hover', 'bb-vapor-modules-pro' ),
 							'show_reset' => true,
+							'show_alpha' => true,
+							'default'    => '000000',
 						),
-						'button_border_width' => array(
-							'type' => 'unit',
-							'label' => __( 'Button Border Width', 'bb-vapor-modules-pro' ),
-							'default' => '2',
+						'button_border_width'       => array(
+							'type'        => 'unit',
+							'label'       => __( 'Button Border Width', 'bb-vapor-modules-pro' ),
+							'default'     => '2',
+							'description' => 'px',
 						),
 						'button_border_width_hover' => array(
-							'type' => 'unit',
-							'label' => __( 'Button Border Width on Hover', 'bb-vapor-modules-pro' ),
-							'default' => '0',
+							'type'        => 'unit',
+							'label'       => __( 'Button Border Width on Hover', 'bb-vapor-modules-pro' ),
+							'default'     => '0',
+							'description' => 'px',
 						),
-						'button_padding' => array(
-							'type' => 'dimension',
+						'button_padding'            => array(
+							'type'  => 'dimension',
 							'label' => __( 'Button Padding', 'bb-vapor-modules-pro' ),
 						),
-						'button_typography' => array(
-							'type' => 'typography',
+						'button_typography'         => array(
+							'type'  => 'typography',
 							'label' => __( 'Button Typography', 'bb-vapor-modules-pro' ),
 						),
 					),
--- bbvm-modules/category-grid/includes/loop-settings.php
+++ bbvm-modules/category-grid/includes/loop-settings.php
@@ -10,155 +10,70 @@ FLBuilderModel::default_settings(
 	)
 );
 ?>
-<div id="fl-builder-settings-section-general" class="fl-loop-builder">
-<h3 class="fl-builder-settings-title"><span class="fl-builder-settings-title-text-wrap"><?php esc_html_e( 'Post Type Select', 'bb-vapor-modules-pro' ); ?></span></h3>
+<div id="fl-builder-settings-section-source" class="fl-loop-data-source-select fl-builder-settings-section">
 	<table class="fl-form-table">
 	<?php
+	$tax_types = array();
+	foreach ( FLBuilderLoop::post_types() as $slug => $bbvm_post_type ) {
+		$taxonomies = FLBuilderLoop::taxonomies( $slug );
+		foreach ( $taxonomies as $tax_slug => $bbvm_tax ) {
+			$tax_types[] = 'custom_tax_' . $slug . '_' . $tax_slug;
+		}
+	}
 
-	// Post type.
+	// Data Source.
 	FLBuilder::render_settings_field(
-		'post_type',
+		'category_options',
 		array(
-			'type'  => 'post-type',
-			'label' => __( 'Post Type', 'uabb' ),
-			'help'  => __( 'Choose the post type to display in module.', 'uabb' ),
+			'type'    => 'select',
+			'label'   => __( 'Category Options', 'bb-vapor-modules-pro' ),
+			'default' => 'taxonomy',
+			'options' => array(
+				'taxonomy' => __( 'Select Categories by Taxonomy', 'bb-vapor-modules-pro' ),
+				'custom'   => __( 'Select Categories Manually', 'bb-vapor-modules-pro' ),
+			),
+			'toggle'  => array(
+				'taxonomy' => array(
+					'fields' => array( 'taxonomy_select' ),
+				),
+				'custom' => array(
+					'fields' => $tax_types,
+				),
+			),
 		),
 		$settings
 	);
+
 	?>
 	</table>
 </div>
-<div id="fl-builder-settings-section-masonary_filter" class="bbvm-settings-section">
-	<h3 class="fl-builder-settings-title"><?php esc_html_e( 'Taxonomy Filter', 'bb-vapor-modules-pro' ); ?></h3>
-	<?php foreach ( FLBuilderLoop::post_types() as $slug => $type ) : ?>
-		<table class="fl-form-table fl-loop-builder-masonary_filter fl-loop-builder-<?php echo esc_attr( $slug ); ?>-masonary_filter"
-			<?php
-			if ( $slug === $settings->post_type ) {
-				echo 'style="display:table;"';
-			}
-			?>
-		>
-		<?php
-
-		// Taxonomies.
-		$taxonomies       = FLBuilderLoop::taxonomies( $slug );
-		$taxonomies_array = array();
-		$toggleArray      = array();
-
-		if ( count( $taxonomies ) > 0 ) {
-			$taxonomies_array[-1] = esc_html__( 'No Filter', 'bb-vapor-modules-pro' );
-		}
-
-		foreach ( $taxonomies as $tax_slug => $tax ) {
-			$taxonomies_array[ $tax_slug ] = $tax->label;
-		}
-
-		if ( count( $taxonomies_array ) > 0 ) {
-			// Taxonomy Filter.
-			FLBuilder::render_settings_field(
-				'masonary_filter_' . $slug,
-				array(
-					'type'    => 'select',
-					'label'   => __( 'Taxonomy Filter', 'uabb' ),
-					'help'    => __( 'Select post filter criteria to display post filter at top of the module.', 'uabb' ),
-					'options' => $taxonomies_array,
-				),
-				$settings
-			);
-		}
-			FLBuilder::render_settings_field(
-				'uabb_masonary_filter_type_' . $slug, array(
-					'type'    => 'select',
-					'label'   => __( 'Select Filter Layout', 'uabb' ),
-					'options' => array(
-						'buttons'   => __( 'Button', 'uabb' ),
-						'drop-down' => __( 'Drop Down', 'uabb' ),
-					),
-				), $settings
-			);
-
-			FLBuilder::render_settings_field(
-				'uabb_masonary_filter_all_edit_' . $slug, array(
-					'type'        => 'text',
-					'label'       => __( '"All" Filter Label', 'uabb' ),
-					'default'     => __( 'All', 'uabb' ),
-					'placeholder' => __( 'All', 'uabb' ),
-					'connections' => array( 'string' ),
-				), $settings
-			);
+<div id="fl-builder-settings-section-filter" class="bbvm-settings-section">
+	<?php
+	foreach ( FLBuilderLoop::post_types() as $slug => $bbvm_post_type ) :
 		?>
-		</table>
-	<?php endforeach; ?>
-</div>
-
-<div id="fl-builder-settings-section-filter" class="uabb-settings-section">
-<h3 class="fl-builder-settings-title"><span class="fl-builder-settings-title-text-wrap"><?php _e( 'Filter', 'uabb' ); ?></span></h3>
-
-	<?php foreach ( FLBuilderLoop::post_types() as $slug => $type ) : ?>
-		<table class="fl-form-table fl-loop-builder-filter fl-loop-builder-<?php echo $slug; ?>-filter"
+		<table class="fl-form-table">
 			<?php
-			if ( $slug == $settings->post_type ) {
-				echo 'style="display:table;"';
-			}
-			?>
-		>
-		<?php
+			// Taxonomies.
+			$taxonomies       = FLBuilderLoop::taxonomies( $slug );
+			$taxonomies_array = array();
 
-		FLBuilder::render_settings_field(
-			'posts_' . $slug . '_matching', array(
-				'type'    => 'select',
-				'label'   => $type->label,
-				'options' => array(
-					'1' => sprintf( /* translators: %s: search term */ __( 'Match these %s', '%s is an object like posts or taxonomies.', 'uabb' ), $type->label, $type->label ),
-					'0' => sprintf( /* translators: %s: search term */ __( 'Do not match these %s', '%s is an object like posts or taxonomies.', 'uabb' ), $type->label, $type->label ),
-
-				),
-				'help'    => sprintf( /* translators: %1$s: search term, translators: %2$s: search term */ __( 'Enter a comma separated list of %1$s. Only these %2$s will be shown.', 'uabb' ), $type->label, $type->label ),
-			), $settings
-		);
-		// Posts.
-		FLBuilder::render_settings_field(
-			'posts_' . $slug, array(
-				'type'   => 'suggest',
-				'action' => 'fl_as_posts',
-				'data'   => $slug,
-				'label'  => '&nbsp',
-			), $settings
-		);
-
-		// Taxonomies.
-		$taxonomies       = FLBuilderLoop::taxonomies( $slug );
-		$taxonomies_array = array();
-
-		foreach ( $taxonomies as $tax_slug => $tax ) {
-
-			FLBuilder::render_settings_field(
-				'tax_' . $slug . '_' . $tax_slug . '_matching', array(
-					'type'    => 'select',
-					'label'   => $tax->label,
-					'help'    => sprintf( /* translators: %1$s: search term, translators: %2$s: search term */ __( 'Enter a comma separated list of %1$s. Only posts with these %2$s will be shown.', 'uabb' ), $tax->label, $tax->label ),
-					'options' => array(
-						'1' => sprintf( /* translators: %s: search term */ __( 'Match these %s', '%s is an object like posts or taxonomies.', 'uabb' ), $tax->label, $tax->label ),
-						'0' => sprintf( /* translators: %s: search term */ __( 'Do not match these %s', '%s is an object like posts or taxonomies.', 'uabb' ), $tax->label, $tax->label ),
-
+			foreach ( $taxonomies as $tax_slug => $bbvm_tax ) {
+				FLBuilder::render_settings_field(
+					'custom_tax_' . $slug . '_' . $tax_slug,
+					array(
+						'type'   => 'suggest',
+						'action' => 'fl_as_terms',
+						'data'   => $tax_slug,
+						'label'  => __( 'Select a term:', 'bb-vapor-modules-pro' ) . ' ' . $bbvm_tax->label,
 					),
-					'help'    => sprintf( /* translators: %1$s: search term, translators: %2$s: search term */ __( 'Enter a comma separated list of %1$s. Only posts with these %2$s will be shown.', 'uabb' ), $tax->label, $tax->label ),
-				), $settings
-			);
-			FLBuilder::render_settings_field(
-				'tax_' . $slug . '_' . $tax_slug, array(
-					'type'   => 'suggest',
-					'action' => 'fl_as_terms',
-					'data'   => $tax_slug,
-					'label'  => '&nbsp',
-				), $settings
-			);
-			$taxonomies_array[ $tax_slug ] = $tax->label;
-		}
+					$settings
+				);
+				$taxonomies_array[ $tax_slug ] = $bbvm_tax->label;
+			}
 
-		?>
+			?>
 		</table>
-	<?php endforeach; ?>
-</div>
-
+		<?php
+	endforeach;
+	?>
 </div>
