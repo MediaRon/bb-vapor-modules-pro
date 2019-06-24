402ca8d17c0ac8c04a6deeac8f5fe7362373a5c9
Ronald Huereca <ronald@mediaron.com>
2019-06-23 19:40:51 -0500

    Code cleanup for FAQ module.

--- bbvm-modules/faq/includes/frontend.js.php
+++ bbvm-modules/faq/includes/frontend.js.php
@@ -1,20 +1,19 @@
 <?php
-if( 'yes' !== $settings->expanded ) :
-?>
-jQuery( document ).ready( function( $ ) {
-	$( '.bbvm-faq-item .bbvm-faq-heading' ).toggle( function( e ) {
-			e.preventDefault();
-			$( this ).find('i').removeClass( 'fa-plus' ).addClass( 'fa-minus' );
-			console.log( $( this ).siblings() );
-			$( this ).siblings('.bbvm-faq-content').fadeIn('slow');
-		},
-		function( e ) {
-			e.preventDefault();
-			$( this ).find('i').removeClass( 'fa-minus' ).addClass( 'fa-plus' );
-			$( this ).siblings('.bbvm-faq-content').fadeOut('slow');
-		}
-	);
-} );
-<?php
+if ( 'yes' !== $settings->expanded ) :
+	?>
+	jQuery( document ).ready( function( $ ) {
+		$( '.bbvm-faq-item .bbvm-faq-heading' ).toggle( function( e ) {
+				e.preventDefault();
+				$( this ).find('i').removeClass( 'fa-plus' ).addClass( 'fa-minus' );
+				console.log( $( this ).siblings() );
+				$( this ).siblings('.bbvm-faq-content').fadeIn('slow');
+			},
+			function( e ) {
+				e.preventDefault();
+				$( this ).find('i').removeClass( 'fa-minus' ).addClass( 'fa-plus' );
+				$( this ).siblings('.bbvm-faq-content').fadeOut('slow');
+			}
+		);
+	} );
+	<?php
 endif;
-?>
--- bbvm-modules/faq/includes/frontend.php
+++ bbvm-modules/faq/includes/frontend.php
@@ -1,22 +1,22 @@
 <div class="fl-bbvm-faq-for-beaverbuilder">
 <?php
-if( 'yes' === $settings->toc && count( $settings->faq ) > 0 ) :
+if ( 'yes' === $settings->toc && count( $settings->faq ) > 0 ) :
 	?>
 	<ul class="bbvm-faq">
 	<?php
-	foreach( $settings->faq as $faq ):
-		printf( '<li><a href="#%s">%s</a></li>', sanitize_title( $faq->faq ), esc_html( $faq->faq ) );
+	foreach ( $settings->faq as $faq ) :
+		printf( '<li><a href="#%s">%s</a></li>', esc_attr( sanitize_title( $faq->faq ) ), esc_html( $faq->faq ) );
 	endforeach;
 	?>
 	</ul>
 	<?php
 endif;
-foreach( $settings->faq as $faq ):
+foreach ( $settings->faq as $faq ) :
 	?>
 	<div class="bbvm-faq-item">
-		<div class="bbvm-faq-heading" id="<?php echo sanitize_title( $faq->faq ); ?>"><?php echo esc_html( $faq->faq ); ?><?php echo 'no' === $settings->expanded ? '<i class="fas fa-plus"></i>' : ''; ?></div>
+		<div class="bbvm-faq-heading" id="<?php echo esc_attr( sanitize_title( $faq->faq ) ); ?>"><?php echo esc_html( $faq->faq ); ?><?php echo 'no' === $settings->expanded ? '<i class="fas fa-plus"></i>' : ''; ?></div>
 		<div class="bbvm-faq-content" <?php echo 'yes' === $settings->expanded ? '' : "style='display:none;'"; ?>>
-			<?php echo $faq->faq_text; ?>
+			<?php echo wp_kses_post( $faq->faq_text ); ?>
 		</div>
 	</div>
 	<?php
