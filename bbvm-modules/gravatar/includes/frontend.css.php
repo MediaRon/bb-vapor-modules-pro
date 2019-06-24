29d8e1c437f0681b954203b88589270d0db01cfd
Ronald Huereca <ronald@mediaron.com>
2019-06-23 21:41:40 -0500

    Code cleanup for Gravatar module.

--- bbvm-modules/gravatar/includes/frontend.css.php
+++ bbvm-modules/gravatar/includes/frontend.css.php
@@ -1,23 +1,24 @@
 <?php
-if( 'circular' === $settings->avatar_type ):
-?>
-.fl-node-<?php echo $id; ?> .fl-bbvm-gravatar img {
-	width: <?php echo absint( $settings->avatar_size ); ?>px;
-	height: <?php echo absint( $settings->avatar_size ); ?>px;
-	min-width: <?php echo absint( $settings->avatar_size ); ?>px;
-	min-height: <?php echo absint( $settings->avatar_size ); ?>px;
-	border-radius: 100%;
-}
-<?php
+if ( 'circular' === $settings->avatar_type ) :
+	?>
+	.fl-node-<?php echo esc_html( $id ); ?> .fl-bbvm-gravatar img {
+		width: <?php echo absint( $settings->avatar_size ); ?>px;
+		height: <?php echo absint( $settings->avatar_size ); ?>px;
+		min-width: <?php echo absint( $settings->avatar_size ); ?>px;
+		min-height: <?php echo absint( $settings->avatar_size ); ?>px;
+		border-radius: 100%;
+	}
+	<?php
 endif;
 ?>
-.fl-node-<?php echo $id; ?> .fl-bbvm-gravatar-for-beaverbuilder {
-	text-align: <?php echo $settings->avatar_align; ?>;
+.fl-node-<?php echo esc_html( $id ); ?> .fl-bbvm-gravatar-for-beaverbuilder {
+	text-align: <?php echo esc_html( $settings->avatar_align ); ?>;
 }
 <?php
-FLBuilderCSS::typography_field_rule( array(
-	'settings'	=> $settings,
-	'setting_name' 	=> 'avatar_typography',
-	'selector' 	=> ".fl-node-$id .fl-bbvm-gravatar-for-beaverbuilder h2"
-) );
-?>
+FLBuilderCSS::typography_field_rule(
+	array(
+		'settings'     => $settings,
+		'setting_name' => 'avatar_typography',
+		'selector'     => ".fl-node-$id .fl-bbvm-gravatar-for-beaverbuilder h2",
+	)
+);
