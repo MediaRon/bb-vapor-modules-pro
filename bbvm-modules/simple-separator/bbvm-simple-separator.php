c3814762f5804179d0a484a4faa2a5e7abdb7d7a
Ronald Huereca <ronald@mediaron.com>
2019-06-24 16:55:20 -0500

    Code cleanup for Simple Separator.

--- bbvm-modules/simple-separator/bbvm-simple-separator.php
+++ bbvm-modules/simple-separator/bbvm-simple-separator.php
@@ -1,69 +1,72 @@
-<?php
+<?php // phpcs:ignore
 class BBVapor_Simple_Separator_Module extends FLBuilderModule {
-	public function __construct()
-	{
-		parent::__construct(array(
-			'name'            => __( 'Simple Separator', 'bb-vapor-modules-pro' ),
-			'description'     => __( 'Simple Separator for Beaver Builder', 'bb-vapor-modules-pro' ),
-			'category'        => __( 'Separators/Spacers', 'bb-vapor-modules-pro' ),
-			'group'           => __( 'Vapor', 'bb-vapor-modules-pro' ),
-			'dir'             => BBVAPOR_PRO_BEAVER_BUILDER_DIR . 'bbvm-modules/simple-separator/',
-			'url'             => BBVAPOR_PRO_BEAVER_BUILDER_URL . 'bbvm-modules/simple-separator/',
-			'editor_export'   => true, // Defaults to true and can be omitted.
-			'enabled'         => true, // Defaults to true and can be omitted.
-			'partial_refresh' => false, // Defaults to false and can be omitted.
-		));
+	public function __construct() {
+		parent::__construct(
+			array(
+				'name'            => __( 'Simple Separator', 'bb-vapor-modules-pro' ),
+				'description'     => __( 'Simple Separator for Beaver Builder', 'bb-vapor-modules-pro' ),
+				'category'        => __( 'Separators/Spacers', 'bb-vapor-modules-pro' ),
+				'group'           => __( 'Vapor', 'bb-vapor-modules-pro' ),
+				'dir'             => BBVAPOR_PRO_BEAVER_BUILDER_DIR . 'bbvm-modules/simple-separator/',
+				'url'             => BBVAPOR_PRO_BEAVER_BUILDER_URL . 'bbvm-modules/simple-separator/',
+				'editor_export'   => true, // Defaults to true and can be omitted.
+				'enabled'         => true, // Defaults to true and can be omitted.
+				'partial_refresh' => false, // Defaults to false and can be omitted.
+			)
+		);
 	}
 }
 /**
  * Register the module and its form settings.
  */
-FLBuilder::register_module('BBVapor_Simple_Separator_Module', array(
-	'general'       => array( // Tab
-		'title'         => __('General', 'bb-vapor-modules-pro'), // Tab title
-		'sections'      => array( // Tab Sections
-			'general'       => array( // Section
-				'title'         => __( 'Separator', 'bb-vapor-modules-pro' ), // Section Title
-				'fields'        => array( // Section Fields
-					'height' => array(
-						'type'          => 'unit',
-						'label'         => __( 'Height of Separator', 'bb-vapor-modules-pro' ),
-						'description'   => 'px',
-						'default'       => '2',
+FLBuilder::register_module(
+	'BBVapor_Simple_Separator_Module',
+	array(
+		'general' => array( // Tab
+			'title'    => __( 'General', 'bb-vapor-modules-pro' ), // Tab title
+			'sections' => array( // Tab Sections
+				'general' => array( // Section
+					'title'  => __( 'Separator', 'bb-vapor-modules-pro' ), // Section Title
+					'fields' => array( // Section Fields
+						'height' => array(
+							'type'        => 'unit',
+							'label'       => __( 'Height of Separator', 'bb-vapor-modules-pro' ),
+							'description' => 'px',
+							'default'     => '2',
+						),
+						'color'  => array(
+							'type'       => 'color',
+							'label'      => __( 'Color of Separator', 'bb-vapor-modules-pro' ),
+							'default'    => '000000',
+							'show_reset' => true,
+							'show_alpha' => true,
+						),
+						'width'  => array(
+							'type'        => 'unit',
+							'label'       => __( 'Width of Separator', 'bb-vapor-modules-pro' ),
+							'default'     => '100',
+							'description' => '%',
+						),
+						'height' => array(
+							'type'        => 'unit',
+							'label'       => __( 'Height of Separator', 'bb-vapor-modules-pro' ),
+							'default'     => '2',
+							'description' => 'px',
+						),
+						'style'  => array(
+							'type'    => 'select',
+							'label'   => __( 'Separator style', 'bb-vapor-modules-pro' ),
+							'default' => 'solid',
+							'options' => array(
+								'solid'  => __( 'Solid', 'bb-vapor-modules-pro' ),
+								'dashed' => __( 'Dashed', 'bb-vapor-modules-pro' ),
+								'dotted' => __( 'Dotted', 'bb-vapor-modules-pro' ),
+								'double' => __( 'Double', 'bb-vapor-modules-pro' ),
+							),
+						),
 					),
-					'color' => array(
-						'type'          => 'color',
-						'label'         => __( 'Color of Separator', 'bb-vapor-modules-pro' ),
-						'default'       => '000000',
-						'show_reset'    => true,
-						'show_alpha'    => true
-					),
-					'width' => array(
-						'type'          => 'unit',
-						'label'         => __( 'Width of Separator', 'bb-vapor-modules-pro' ),
-						'default'       => '100',
-						'description'   => '%'
-					),
-					'height' => array(
-						'type'          => 'unit',
-						'label'         => __( 'Height of Separator', 'bb-vapor-modules-pro' ),
-						'default'       => '2',
-						'description'   => 'px'
-					),
-					'style' => array(
-						'type'          => 'select',
-						'label'         => __( 'Separator style', 'bb-vapor-modules-pro' ),
-						'default'       => 'solid',
-						'options'       => array(
-							'solid'  => __( 'Solid', 'bb-vapor-modules-pro' ),
-							'dashed' => __( 'Dashed', 'bb-vapor-modules-pro' ),
-							'dotted' => __( 'Dotted', 'bb-vapor-modules-pro' ),
-							'double' => __( 'Double', 'bb-vapor-modules-pro' ),
-						)
-					),
-
-				)
-			)
-		)
-	),
-) );
+				),
+			),
+		),
+	)
+);
--- bbvm-modules/simple-separator/includes/frontend.css.php
+++ bbvm-modules/simple-separator/includes/frontend.css.php
@@ -1,8 +1,8 @@
-.fl-node-<?php echo $id; ?> .fl-bbvm-simple-separator {
+.fl-node-<?php echo esc_html( $id ); ?> .fl-bbvm-simple-separator {
 	clear: both;
 	width: <?php echo absint( $settings->width ); ?>%;
-	border-top-color: <?php echo (6 == strlen( $settings->color ) ) ? '#' . $settings->color : $settings->color; ?>;
-	border-top-style: <?php echo $settings->style; ?>;
+	border-top-color: <?php echo esc_html( BBVapor_Modules_Pro::get_color( $settings->color ) ); ?>;
+	border-top-style: <?php echo esc_html( $settings->style ); ?>;
 	border-top-width: <?php echo absint( $settings->height ); ?>px;
 	background: transparent;
 }
