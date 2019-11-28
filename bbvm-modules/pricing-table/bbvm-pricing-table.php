79b997fdb6f56f99312a0fd4f0dd83111763b515
Ronald Huereca <rhuereca@updraftplus.com>
2019-11-28 17:57:28 -0600

    Adding feature styles.

--- bbvm-modules/pricing-table/bbvm-pricing-table.php
+++ bbvm-modules/pricing-table/bbvm-pricing-table.php
@@ -491,296 +491,43 @@ FLBuilder::register_module(
 				),
 			),
 		),
-		'style'   => array(
-			'title'    => __( 'Style', 'bb-vapor-modules-pro' ),
+		'styles'  => array(
+			'title'    => __( 'Styles', 'bb-vapor-modules-pro' ),
 			'sections' => array(
-				'style' => array(
-					'title'  => __( 'Style', 'bb-vapor-modules-pro' ),
+				'features' => array(
+					'title'  => __( 'Features', 'bb-vapor-modules-pro' ),
 					'fields' => array(
-						'blockquote_style'              => array(
-							'type'    => 'select',
-							'label'   => __( 'Blockquote Style Select', 'bb-vapor-modules-pro' ),
-							'options' => array(
-								'none'                 => __( 'None', 'bb-vapor-modules-pro' ),
-								'enhanced'             => __( 'Enhanced', 'bb-vapor-modules-pro' ),
-								'background_quotes'    => __( 'Background Quotes', 'bb-vapor-modules-pro' ),
-								'left_border'          => __( 'Left Border', 'bb-vapor-modules-pro' ),
-								'right_border'         => __( 'Right Border', 'bb-vapor-modules-pro' ),
-								'top_bottom_border'    => __( 'Top and Bottom Border', 'bb-vapor-modules-pro' ),
-								'cite'                 => __( 'Cite', 'bb-vapor-modules-pro' ),
-								'cite_border_effect'   => __( 'Cite Border Effect', 'bb-vapor-modules-pro' ),
-								'cite_animated_border' => __( 'Cite Animated Border', 'bb-vapor-modules-pro' ),
-								'cite_scale'           => __( 'Cite Scale', 'bb-vapor-modules-pro' ),
-								'cite_brackets'        => __( 'Cite Brackets', 'bb-vapor-modules-pro' ),
-								'cite_icon'            => __( 'Cite Icon', 'bb-vapor-modules-pro' ),
-							),
-							'default' => 'none',
-							'toggle'  => array(
-								'bold'                 => array(
-									'fields' => array(
-										'bold_quote_color',
-										'bold_background_color',
-										'bold_text_shadow_color',
-									),
-								),
-								'enhanced'             => array(
-									'fields' => array(
-										'enhanced_background_color',
-										'enhanced_quote_color',
-										'enhanced_text_color',
-										'enhanced_border_color',
-										'enhanced_gradient',
-									),
-								),
-								'left_border'          => array(
-									'fields' => array(
-										'border_type',
-										'border_width',
-										'background_color',
-										'text_color',
-									),
-								),
-								'right_border'         => array(
-									'fields' => array(
-										'border_type',
-										'border_width',
-										'background_color',
-										'text_color',
-									),
-								),
-								'top_bottom_border'    => array(
-									'fields' => array(
-										'top_bottom_border_color',
-										'top_bottom_border_color_hover',
-										'top_bottom_border_height',
-										'background_color',
-										'text_color',
-									),
-								),
-								'cite'                 => array(
-									'fields' => array(
-										'cite_border_color',
-										'text_color',
-									),
-								),
-								'cite_border_effect'   => array(
-									'fields' => array(
-										'cite_border_color',
-										'background_color',
-										'text_color',
-									),
-								),
-								'cite_animated_border' => array(
-									'fields' => array(
-										'cite_border_color',
-										'background_color',
-										'text_color',
-									),
-								),
-								'cite_scale'           => array(
-									'fields' => array(
-										'background_color',
-										'text_color',
-									),
-								),
-								'cite_brackets'        => array(
-									'fields' => array(
-										'cite_border_color',
-										'background_color',
-										'text_color',
-									),
-								),
-								'cite_icon'            => array(
-									'fields' => array(
-										'cite_border_color',
-										'background_color',
-										'text_color',
-									),
-								),
-								'background_quotes'    => array(
-									'fields' => array(
-										'background_quote_color',
-										'background_color',
-										'text_color',
-									),
-								),
-							),
-						),
-						'background_quote_color'        => array(
+						'odd_color'       => array(
 							'type'       => 'color',
-							'label'      => __( 'Background Quote Color', 'bb-vapor-modules-pro' ),
-							'show_alpha' => true,
-							'show_reset' => true,
-							'default'    => 'rgba(0, 0, 0, 1)',
-						),
-						'bold_quote_color'              => array(
-							'type'       => 'color',
-							'label'      => __( 'Bold Quote Color', 'bb-vapor-modules-pro' ),
-							'show_alpha' => true,
-							'show_reset' => true,
-							'default'    => 'rgba(206, 255, 246, 0.8)',
-						),
-						'bold_text_shadow_color'        => array(
-							'type'       => 'color',
-							'label'      => __( 'Text Shadow Color', 'bb-vapor-modules-pro' ),
-							'show_alpha' => true,
-							'show_reset' => true,
-							'default'    => 'rgba(206, 255, 246, 0.2)',
-						),
-						'bold_background_color'         => array(
-							'type'       => 'color',
-							'label'      => __( 'Background Color', 'bb-vapor-modules-pro' ),
-							'show_alpha' => true,
-							'show_reset' => true,
-							'default'    => 'FFFFFF',
-						),
-						'enhanced_gradient'             => array(
-							'type'  => 'gradient',
-							'label' => __( 'Container Gradient', 'bb-vapor-modules-pro' ),
-						),
-						'enhanced_background_color'     => array(
-							'type'       => 'color',
-							'label'      => __( 'Background Color', 'bb-vapor-modules-pro' ),
+							'label'      => __( 'Odd Color Background', 'bb-vapor-modules-pro' ),
 							'show_alpha' => true,
 							'show_reset' => true,
 						),
-						'enhanced_quote_color'          => array(
+						'odd_color_text'  => array(
 							'type'       => 'color',
-							'label'      => __( 'Enhanced Quote Color', 'bb-vapor-modules-pro' ),
+							'label'      => __( 'Odd Text Color', 'bb-vapor-modules-pro' ),
+							'show_reset' => true,
+						),
+						'even_color'      => array(
+							'type'       => 'color',
+							'label'      => __( 'Even Color Background', 'bb-vapor-modules-pro' ),
 							'show_alpha' => true,
 							'show_reset' => true,
-							'default'    => 'rgba(0,0,0,1)',
 						),
-						'enhanced_text_color'           => array(
+						'even_color_text' => array(
 							'type'       => 'color',
-							'label'      => __( 'Enhanced Text Color', 'bb-vapor-modules-pro' ),
-							'show_alpha' => true,
+							'label'      => __( 'Even Text Color', 'bb-vapor-modules-pro' ),
 							'show_reset' => true,
-							'default'    => '000000',
 						),
-						'enhanced_border_color'         => array(
-							'type'       => 'color',
-							'label'      => __( 'Enhanced Border Color', 'bb-vapor-modules-pro' ),
-							'show_reset' => true,
-							'default'    => '000000',
-						),
-						'font_heading'                  => array(
-							'type'    => 'font',
-							'label'   => __( 'Heading Font', 'bb-vapor-modules-pro' ),
-							'default' => array(
-								'family' => 'Montserrat',
-								'weight' => 700,
-							),
-						),
-						'font_size'                     => array(
-							'type'        => 'unit',
-							'label'       => __( 'Heading Font Size', 'bb-vapor-modules-pro' ),
-							'description' => 'px',
-							'default'     => '32',
-							'responsive'  => true,
-						),
-						'font_attribution'              => array(
-							'type'    => 'font',
-							'label'   => __( 'Attribution Font', 'bb-vapor-modules-pro' ),
-							'default' => array(
-								'family' => 'Oswald',
-								'weight' => 400,
-							),
-						),
-						'font_size_attribution'         => array(
-							'type'        => 'unit',
-							'label'       => __( 'Attribution Font Size', 'bb-vapor-modules-pro' ),
-							'description' => 'px',
-							'default'     => '24',
-							'responsive'  => true,
-						),
-						'border_type'                   => array(
-							'type'    => 'select',
-							'label'   => __( 'Border Type', 'bb-vapor-modules-pro' ),
-							'options' => array(
-								'color'    => __( 'Color', 'bb-vapor-modules-pro' ),
-								'gradient' => __( 'Gradient', 'bb-vapor-modules-pro' ),
-							),
-							'toggle'  => array(
-								'color'    => array(
-									'fields' => array(
-										'border_background_color',
-									),
-								),
-								'gradient' => array(
-									'fields' => array(
-										'border_background_gradient',
-									),
-								),
-							),
-						),
-						'border_background_color'       => array(
-							'type'       => 'color',
-							'label'      => __( 'Border Background Color', 'bb-vapor-modules-pro' ),
-							'show_alpha' => true,
-							'show_reset' => true,
-							'default'    => '333333',
-						),
-						'border_background_gradient'    => array(
-							'type'  => 'gradient',
-							'label' => __( 'Border Background Gradient', 'bb-vapor-modules-pro' ),
-						),
-						'border_width'                  => array(
-							'type'        => 'unit',
-							'label'       => __( 'Border Width', 'bb-vapor-modules-pro' ),
-							'description' => 'px',
-							'default'     => '30',
-						),
-						'top_bottom_border_color'       => array(
-							'type'       => 'color',
-							'label'      => __( 'Border Color', 'bb-vapor-modules-pro' ),
-							'show_alpha' => true,
-							'show_reset' => true,
-							'default'    => 'FAFAFA',
-						),
-						'top_bottom_border_color_hover' => array(
-							'type'       => 'color',
-							'label'      => __( 'Border Color on Hover', 'bb-vapor-modules-pro' ),
-							'show_alpha' => true,
-							'show_reset' => true,
-							'default'    => 'FFFFFF',
-						),
-						'top_bottom_border_height'      => array(
-							'type'    => 'unit',
-							'label'   => __( 'Border Height', 'bb-vapor-modules-pro' ),
-							'default' => '2',
-						),
-						'cite_border_color'             => array(
-							'type'       => 'color',
-							'label'      => __( 'Cite Border Color', 'bb-vapor-modules-pro' ),
-							'show_reset' => true,
-							'default'    => '757575',
-						),
-						'background_color'              => array(
-							'type'       => 'color',
-							'label'      => __( 'Background Color', 'bb-vapor-modules-pro' ),
-							'show_alpha' => true,
-							'show_reset' => true,
-							'default'    => 'FFFFFF',
-						),
-						'text_color'                    => array(
-							'type'       => 'color',
-							'label'      => __( 'Text Color', 'bb-vapor-modules-pro' ),
-							'show_alpha' => true,
-							'show_reset' => true,
-							'default'    => '000000',
-						),
-						'blockquote_padding'            => array(
+						'feature_padding' => array(
 							'type'       => 'dimension',
-							'label'      => __( 'Blockquote Padding', 'bb-vapor-modules-pro' ),
+							'label'      => __( 'Feature Padding', 'bb-vapor-modules-pro' ),
 							'responsive' => true,
 						),
-						'max_width'                     => array(
-							'type'        => 'unit',
-							'label'       => __( 'Max Width', 'bb-vapor-modules-pro' ),
-							'responsive'  => true,
-							'default'     => '50',
-							'description' => '%',
+						'feature_border'  => array(
+							'type'       => 'border',
+							'label'      => __( 'Feature Border', 'bb-vapor-modules-pro' ),
+							'responsive' => true,
 						),
 					),
 				),
