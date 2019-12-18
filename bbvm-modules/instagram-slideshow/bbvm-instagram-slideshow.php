60873d522cef13f294a91a5e88b53e9808fff5dc
Ronald Huereca <rhuereca@updraftplus.com>
2019-12-17 20:33:28 -0600

    Initial commit of Instagram Slideshow.

---
+++ bbvm-modules/instagram-slideshow/bbvm-instagram-slideshow.php
@@ -0,0 +1,249 @@
+<?php // phpcs:ignore
+class BBVapor_Instagram_Slideshow extends FLBuilderModule {
+	/**
+	 * Class constructor.
+	 */
+	public function __construct() {
+		/**
+		 * Class constructor.
+		 */
+		parent::__construct(
+			array(
+				'name'            => __( 'Instagram Slideshow', 'bb-vapor-modules-pro' ),
+				'description'     => __( 'Add an Instagram Slideshow', 'bb-vapor-modules-pro' ),
+				'category'        => __( 'Base', 'bb-vapor-modules-pro' ),
+				'group'           => apply_filters( 'bbvm_whitelabel_category', __( 'Vapor', 'bb-vapor-modules-pro' ) ),
+				'dir'             => BBVAPOR_PRO_BEAVER_BUILDER_DIR . 'bbvm-modules/instagram-slideshow/',
+				'url'             => BBVAPOR_PRO_BEAVER_BUILDER_URL . 'bbvm-modules/instagram-slideshow/',
+				'editor_export'   => true, // Defaults to true and can be omitted.
+				'enabled'         => true, // Defaults to true and can be omitted.
+				'partial_refresh' => false, // Defaults to false and can be omitted.
+			)
+		);
+	}
+}
+/**
+ * Register the module and its form settings.
+ */
+FLBuilder::register_module(
+	'BBVapor_Instagram_Slideshow',
+	array(
+		'general'    => array(
+			'title'    => __( 'Instagram', 'bb-vapor-modules-pro' ),
+			'sections' => array(
+				'general' => array(
+					'title'  => __( 'Instagram Settings', 'bb-vapor-modules-pro' ),
+					'fields' => array(
+						'items_show'                    => array(
+							'type'    => 'unit',
+							'label'   => __( 'Number of Instagram Photos to Show', 'bb-vapor-modules-pro' ),
+							'default' => '10',
+						),
+						'layout'                        => array(
+							'type'        => 'select',
+							'label'       => __( 'Display Options', 'bb-vapor-modules-pro' ),
+							'options'     => array(
+								'card'    => __( 'Card', 'bb-vapor-modules-pro' ),
+								'masonry' => __( 'Masonry', 'bb-vapor-modules-pro' ),
+							),
+							'default'     => 'card',
+							'toggle'      => array(
+								'card' => array(
+									'fields' => array(
+										'card_columns',
+									),
+								),
+							),
+							'description' => __( 'You may need to save and refresh to see the new layout.', 'bb-vapor-modules-pro' ),
+						),
+						'lightbox'                      => array(
+							'type'    => 'select',
+							'label'   => __( 'Pop Up Images in a Lightbox', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+							),
+							'default' => 'no',
+						),
+						'card_columns'                  => array(
+							'type'    => 'select',
+							'label'   => __( 'Number of Card Columns', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'1' => '1',
+								'2' => '2',
+								'3' => '3',
+								'4' => '4',
+							),
+							'default' => '3',
+						),
+						'load_images_background_image'  => array(
+							'type'    => 'select',
+							'label'   => __( 'Load Images as a Background Image', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+							),
+							'default' => 'no',
+							'help'    => __( 'This option is useful for Grid layouts', 'bb-vapor-modules-pro' ),
+							'toggle'  => array(
+								'yes' => array(
+									'fields' => array(
+										'background_min_height',
+									),
+								),
+							),
+						),
+						'background_min_height_desktop' => array(
+							'type'        => 'unit',
+							'label'       => __( 'Background Image Minimum Height on Desktop', 'bb-vapor-modules-pro' ),
+							'default'     => '350',
+							'description' => 'px',
+						),
+						'background_min_height_tablet'  => array(
+							'type'        => 'unit',
+							'label'       => __( 'Background Image Minimum Height on Tablet', 'bb-vapor-modules-pro' ),
+							'default'     => '300',
+							'description' => 'px',
+						),
+						'background_min_height_mobile'  => array(
+							'type'        => 'unit',
+							'label'       => __( 'Background Image Minimum Height on Mobile', 'bb-vapor-modules-pro' ),
+							'default'     => '300',
+							'description' => 'px',
+						),
+						'show_likes_comments'           => array(
+							'type'    => 'select',
+							'label'   => __( 'Show Likes and Comments?', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+							),
+							'default' => 'yes',
+						),
+						'show_caption'                  => array(
+							'type'    => 'select',
+							'label'   => __( 'Show Caption?', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+							),
+							'default' => 'yes',
+						),
+						'card_padding'                  => array(
+							'type'       => 'dimension',
+							'label'      => __( 'Card Padding', 'bb-vapor-modules-pro' ),
+							'responsive' => true,
+						),
+						'card_background_color'         => array(
+							'type'       => 'color',
+							'label'      => __( 'Card Background Color', 'bb-vapor-modules-pro' ),
+							'show_reset' => true,
+							'default'    => 'FFFFFF',
+						),
+						'card_text_color'               => array(
+							'type'       => 'color',
+							'label'      => __( 'Card Text Color', 'bb-vapor-modules-pro' ),
+							'show_reset' => true,
+							'default'    => '000000',
+						),
+						'max_image_width'               => array(
+							'type'        => 'unit',
+							'label'       => __( 'Max Card Width', 'bb-vapor-modules-pro' ),
+							'responsive'  => true,
+							'default'     => '1000',
+							'description' => __( 'Can override your column width settings', 'bb-vapor-modules-pro' ),
+						),
+					),
+				),
+			),
+		),
+		'typography' => array(
+			'title'    => __( 'Typography', 'bb-vapor-modules-pro' ),
+			'sections' => array(
+				'typography' => array(
+					'title'  => __( 'Typography Settings', 'bb-vapor-modules-pro' ),
+					'fields' => array(
+						'likes_comments_typography' => array(
+							'type'       => 'typography',
+							'label'      => __( 'Likes and Comments Typography', 'bb-vapor-modules-pro' ),
+							'responsive' => true,
+						),
+						'caption_typography'        => array(
+							'type'       => 'typography',
+							'label'      => __( 'Caption Typography', 'bb-vapor-modules-pro' ),
+							'responsive' => true,
+						),
+					),
+				),
+			),
+		),
+		'buttons'    => array(
+			'title'    => __( 'Buttons', 'bb-vapor-modules-pro' ),
+			'sections' => array(
+				'buttons' => array(
+					'title'  => __( 'Button Settings', 'bb-vapor-modules-pro' ),
+					'fields' => array(
+						'show_load_more_button'      => array(
+							'type'        => 'select',
+							'label'       => __( 'Show Load More Button', 'bb-vapor-modules-pro' ),
+							'options'     => array(
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+							),
+							'default'     => 'yes',
+							'description' => __( 'Showing Load More is not recommended for the Masonry layout', 'bb-vapor-modules-pro' ),
+						),
+						'show_follow_us_button'      => array(
+							'type'    => 'select',
+							'label'   => __( 'Show Follow Us Button', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+							),
+							'default' => 'yes',
+						),
+						'load_more_background_color' => array(
+							'type'       => 'color',
+							'label'      => __( 'Show More Background Color', 'bb-vapor-modules-pro' ),
+							'show_reset' => true,
+							'default'    => '000000',
+						),
+						'load_more_text_color'       => array(
+							'type'       => 'color',
+							'label'      => __( 'Show More Text Color', 'bb-vapor-modules-pro' ),
+							'show_reset' => true,
+							'default'    => 'FFFFFF',
+						),
+						'load_more_text'             => array(
+							'type'    => 'text',
+							'label'   => __( 'Load More Text', 'bb-vapor-modules-pro' ),
+							'default' => __( 'Load More...', 'bb-vapor-modules-pro' ),
+						),
+						'follow_background_color'    => array(
+							'type'       => 'color',
+							'label'      => __( 'Follow Background Color', 'bb-vapor-modules-pro' ),
+							'show_reset' => true,
+							'default'    => '408bd1',
+						),
+						'follow_text_color'          => array(
+							'type'       => 'color',
+							'label'      => __( 'Show More Text Color', 'bb-vapor-modules-pro' ),
+							'show_reset' => true,
+							'default'    => 'FFFFFF',
+						),
+						'follow_text'                => array(
+							'type'    => 'text',
+							'label'   => __( 'Follow Text', 'bb-vapor-modules-pro' ),
+							'default' => __( 'Follow on Instagram', 'bb-vapor-modules-pro' ),
+						),
+						'follow_icon'                => array(
+							'type'        => 'icon',
+							'label'       => __( 'Follow Icon', 'bb-vapor-modules-pro' ),
+							'show_remove' => true,
+						),
+					),
+				),
+			),
+		),
+	)
+);
---
+++ bbvm-modules/instagram-slideshow/includes/frontend.php
@@ -0,0 +1,145 @@
+<?php
+/**
+ * Instagram Module.
+ *
+ * @link https://bbvapormodules.com
+ *
+ * @package BB Vapor Modules
+ * @since 1.3.0
+ */
+
+?>
+<div class="fl-bbvm-instagram-for-beaverbuilder">
+	<?php
+	// Get cache if possible.
+	$instagram = get_option( 'bbvm-modules-instagram', array() );
+	if ( ! isset( $instagram['token'] ) ) {
+		?>
+		<p><?php esc_html_e( 'Please connect to Instagram in the plugin settings.', 'bb-vapor-modules-pro' ); ?>&nbsp;<a href="<?php echo esc_url( add_query_arg( array( 'page' => 'bbvm-for-beaver-builder' ), admin_url( 'options-general.php' ) ) ); ?>"><?php esc_html_e( 'Connect', 'bb-vapor-modules-pro' ); ?></a>
+		<?php
+	} else {
+		$instagram_json = isset( $instagram['json'] ) ? $instagram['json'] : '';
+		$time_now       = time();
+		$break_cache    = false;
+		if ( ( $time_now - ( isset( $instagram['last_cached'] ) ? $instagram['last_cached'] : 0 ) > 3600 ) || FLBuilderModel::is_builder_enabled() ) {
+			$break_cache = true;
+		}
+		if ( empty( $instagram_json ) || $break_cache ) {
+			$sig_response = wp_remote_get( esc_url_raw( sprintf( 'https://mediaron.com/instagram/getsig.php?user_id=%s&token=%s&max_id=%s&feed_count=%s', $instagram['user_id'], $instagram['token'], false, $settings->items_show ) ) );
+			$sig          = wp_remote_retrieve_body( $sig_response );
+
+			$response                 = wp_remote_get( esc_url_raw( sprintf( 'https://api.instagram.com/v1/users/%d/media/recent?access_token=%s&sig=%s&count=%d', $instagram['user_id'], $instagram['token'], $sig, $settings->items_show ) ) );
+			$response_json            = wp_remote_retrieve_body( $response );
+			$instagram['json']        = $response_json;
+			$instagram['last_cached'] = time();
+			update_option( 'bbvm-modules-instagram', $instagram );
+			$instagram_json = json_decode( $response_json );
+		} else {
+			$instagram_json = json_decode( $instagram_json );
+		}
+		?>
+		<section class="fl-node-instagram">
+			<?php
+			// Get profile information.
+			$author_avatar             = '';
+			$author_full_name          = '';
+			$author_instagram_feed_url = '';
+			foreach ( $instagram_json->data as $key => $user_data ) {
+				$author_full_name          = $user_data->user->full_name;
+				$author_avatar             = $user_data->user->profile_picture;
+				$author_instagram_feed_url = sprintf( 'https://instagram.com/%s', $user_data->user->username );
+				break;
+			}
+			?>
+			<div class="instagram-author">
+			<a href="<?php echo esc_url( $author_instagram_feed_url ); ?>"><img src="<?php echo esc_url( $author_avatar ); ?>" alt="<?php echo esc_attr( $author_full_name ); ?>" />&nbsp;<?php echo esc_html( $author_full_name ); ?></a>
+			</div>
+			<div class="fl-node-instafeed <?php echo ( 'card' === $settings->layout ) ? 'instagram-card-wrapper' : 'instagram-masonry-wrapper'; ?>">
+			<?php
+			foreach ( $instagram_json->data as $key => $user_data ) {
+				?>
+				<div class="instagram-card">
+					<div class="instagram-image">
+						<?php
+						if ( 'yes' === $settings->load_images_background_image ) :
+							$instagram_background_image_css = sprintf( 'style="background-image: url(%s); background-size: cover; background-position: center center;"', esc_url( $user_data->images->standard_resolution->url ) );
+							if ( 'yes' === $settings->lightbox ) :
+								?>
+								<a class="bbvm-ig-bgimage bbvm-instagram-lightbox" href="<?php echo esc_url( $user_data->images->standard_resolution->url ); ?>" <?php echo $instagram_background_image_css; // phpcs:ignore ?>></a>
+								<?php
+							else :
+								?>
+								<a class="bbvm-ig-bgimage" href="<?php echo esc_url( $user_data->link ); ?>" <?php echo $instagram_background_image_css; // phpcs:ignore ?>></a>
+								<?php
+							endif;
+							?>
+							<?php
+							else :
+								if ( 'yes' === $settings->lightbox ) :
+									?>
+									<a class="bbvm-instagram-lightbox" href="<?php echo esc_url( $user_data->images->standard_resolution->url ); ?>"><img src="<?php echo esc_url( $user_data->images->standard_resolution->url ); ?>" /></a>
+									<?php
+								else :
+									?>
+									<a href="<?php echo esc_url( $user_data->link ); ?>"><img src="<?php echo esc_url( $user_data->images->standard_resolution->url ); ?>" /></a>
+									<?php
+								endif;
+								?>
+								<?php
+						endif;
+							?>
+					</div>
+					<?php
+					if ( 'yes' === $settings->show_likes_comments ) :
+						?>
+						<div class="instagram-meta">
+							<span class="instagram-likes">Likes: <?php echo esc_html( number_format( $user_data->likes->count ) ); ?></span><span class="instagram-comments"><?php echo esc_html__( 'Comments:', 'bb-vapor-modules-pro' ); ?> <?php echo esc_html( number_format( $user_data->comments->count ) ); ?></span>
+						</div>
+						<?php
+					endif;
+					?>
+					<?php
+					if ( 'yes' === $settings->show_caption ) :
+						?>
+						<p class="instagram-caption"><?php echo esc_html( $user_data->caption->text ); ?></p>
+						<?php
+					endif;
+					?>
+				</div>
+				<?php
+			}
+			?>
+			</div>
+			<?php
+			if ( 'yes' === $settings->show_load_more_button || 'yes' === $settings->show_follow_us_button ) :
+				?>
+				<div class="instagram-buttons">
+				<?php
+				if ( isset( $instagram_json->pagination->next_url ) ) {
+					$sig_response  = wp_remote_get( esc_url_raw( sprintf( 'https://mediaron.com/instagram/getsig.php?user_id=%s&token=%s&max_id=%s&feed_count=%s', $instagram['user_id'], $instagram['token'], $instagram_json->pagination->next_max_id, $settings->items_show ) ) );
+					$sig           = wp_remote_retrieve_body( $sig_response );
+					$load_more_url = sprintf( 'https://api.instagram.com/v1/users/%d/media/recent?access_token=%s&sig=%s&count=%d&max_id=%s', $instagram['user_id'], $instagram['token'], $sig, $settings->items_show, $instagram_json->pagination->next_max_id );
+
+					?>
+					<?php
+					if ( 'yes' === $settings->show_load_more_button ) :
+						?>
+						<a class="load-more"  data-ajax-url="<?php echo esc_url( admin_url( 'admin-ajax.php' ) ); ?>" data-user-id="<?php echo esc_attr( $instagram['user_id'] ); ?>" data-access-token="<?php echo esc_attr( $instagram['token'] ); ?>" data-feed-count="<?php echo esc_attr( $settings->items_show ); ?>" data-lightbox="<?php echo 'yes' === $settings->load_images_background_image ? 'on' : 'off'; ?>" data-background="<?php echo 'yes' === $settings->lightbox ? 'on' : 'off'; ?>" class="btn btn-default" href="<?php echo esc_url_raw( $load_more_url ); ?>"><?php echo esc_html( $settings->load_more_text ); ?></a>
+					<?php endif; ?>
+					<?php
+				}
+				?>
+				<?php
+				if ( 'yes' === $settings->show_follow_us_button ) :
+					?>
+					<a class="instagram-follow" href="<?php echo esc_url( $author_instagram_feed_url ); ?>"><i class="fab fa-instagram"></i> <?php echo esc_html( $settings->follow_text ); ?></a>
+				<?php endif; ?>
+				</div>
+				<?php
+			endif;
+			?>
+		</section>
+		<?php
+	}
+	?>
+</div>
