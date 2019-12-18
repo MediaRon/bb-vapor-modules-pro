bd62a2af88d6db02c8acf43044199335427e6093
Ronald Huereca <rhuereca@updraftplus.com>
2019-12-18 08:04:56 -0600

    Styles and responsive goodies.

--- bbvm-modules/instagram-slideshow/bbvm-instagram-slideshow.php
+++ bbvm-modules/instagram-slideshow/bbvm-instagram-slideshow.php
@@ -28,6 +28,7 @@ class BBVapor_Instagram_Slideshow extends FLBuilderModule {
 		$this->add_js( 'owl-carousel', BBVAPOR_PRO_BEAVER_BUILDER_URL . 'js/owl-carousel/owl.carousel.min.js', array( 'jquery' ), BBVAPOR_PRO_BEAVER_BUILDER_VERSION, true );
 		$this->add_css( 'owl-carousel-css', BBVAPOR_PRO_BEAVER_BUILDER_URL . 'js/owl-carousel/assets/owl.carousel.min.css', array(), BBVAPOR_PRO_BEAVER_BUILDER_VERSION, 'all' );
 		$this->add_css( 'owl-carousel-theme', BBVAPOR_PRO_BEAVER_BUILDER_URL . 'js/owl-carousel/assets/owl.theme.default.min.css', array(), BBVAPOR_PRO_BEAVER_BUILDER_VERSION, 'all' );
+		$this->add_css( 'animate', BBVAPOR_PRO_BEAVER_BUILDER_URL . 'css/animate.css', array(), BBVAPOR_PRO_BEAVER_BUILDER_VERSION, 'all' );
 	}
 }
 /**
@@ -47,22 +48,27 @@ FLBuilder::register_module(
 							'label'   => __( 'Number of Instagram Photos to Show', 'bb-vapor-modules-pro' ),
 							'default' => '10',
 						),
-						'layout'                        => array(
-							'type'        => 'select',
-							'label'       => __( 'Display Options', 'bb-vapor-modules-pro' ),
-							'options'     => array(
-								'card'    => __( 'Card', 'bb-vapor-modules-pro' ),
-								'masonry' => __( 'Masonry', 'bb-vapor-modules-pro' ),
+						'items' => array(
+							'type'    => 'unit',
+							'label'   => __( 'Number of Items in the Carousel', 'bb-vapor-modules-pro' ),
+							'default' => 1,
+							'slider'  => array(
+								'min'  => 1,
+								'max'  => 6,
+								'step' => 1,
 							),
-							'default'     => 'card',
-							'toggle'      => array(
-								'card' => array(
-									'fields' => array(
-										'card_columns',
-									),
-								),
+						),
+						'image_width' => array(
+							'type'        => 'unit',
+							'label'       => __( 'Image Max Width', 'bb-vapor-modules-pro' ),
+							'description' => __( 'This only applies if you have selected one item in the carousel', 'bb-vapor-modules-pro' ),
+							'default'     => 400,
+							'slider'      => array(
+								'min'  => 250,
+								'max'  => 2000,
+								'step' => 10,
 							),
-							'description' => __( 'You may need to save and refresh to see the new layout.', 'bb-vapor-modules-pro' ),
+
 						),
 						'lightbox'                      => array(
 							'type'    => 'select',
@@ -73,134 +79,149 @@ FLBuilder::register_module(
 							),
 							'default' => 'no',
 						),
-						'card_columns'                  => array(
-							'type'    => 'select',
-							'label'   => __( 'Number of Card Columns', 'bb-vapor-modules-pro' ),
-							'options' => array(
-								'1' => '1',
-								'2' => '2',
-								'3' => '3',
-								'4' => '4',
-							),
-							'default' => '3',
-						),
-						'load_images_background_image'  => array(
-							'type'    => 'select',
-							'label'   => __( 'Load Images as a Background Image', 'bb-vapor-modules-pro' ),
-							'options' => array(
-								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
-								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
-							),
-							'default' => 'no',
-							'help'    => __( 'This option is useful for Grid layouts', 'bb-vapor-modules-pro' ),
-							'toggle'  => array(
-								'yes' => array(
-									'fields' => array(
-										'background_min_height',
-									),
-								),
-							),
-						),
-						'background_min_height_desktop' => array(
-							'type'        => 'unit',
-							'label'       => __( 'Background Image Minimum Height on Desktop', 'bb-vapor-modules-pro' ),
-							'default'     => '350',
-							'description' => 'px',
-						),
-						'background_min_height_tablet'  => array(
-							'type'        => 'unit',
-							'label'       => __( 'Background Image Minimum Height on Tablet', 'bb-vapor-modules-pro' ),
-							'default'     => '300',
-							'description' => 'px',
-						),
-						'background_min_height_mobile'  => array(
-							'type'        => 'unit',
-							'label'       => __( 'Background Image Minimum Height on Mobile', 'bb-vapor-modules-pro' ),
-							'default'     => '300',
-							'description' => 'px',
-						),
-						'show_likes_comments'           => array(
-							'type'    => 'select',
-							'label'   => __( 'Show Likes and Comments?', 'bb-vapor-modules-pro' ),
-							'options' => array(
-								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
-								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
-							),
-							'default' => 'yes',
-						),
-						'show_caption'                  => array(
-							'type'    => 'select',
-							'label'   => __( 'Show Caption?', 'bb-vapor-modules-pro' ),
-							'options' => array(
-								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
-								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
-							),
-							'default' => 'yes',
-						),
-						'card_padding'                  => array(
+						'card_padding'             => array(
 							'type'       => 'dimension',
-							'label'      => __( 'Card Padding', 'bb-vapor-modules-pro' ),
+							'label'      => __( 'Image Padding', 'bb-vapor-modules-pro' ),
 							'responsive' => true,
 						),
-						'card_background_color'         => array(
+						'card_margin'              => array(
+							'type'       => 'dimension',
+							'label'      => __( 'Image Margin', 'bb-vapor-modules-pro' ),
+							'responsive' => true,
+						),
+						'animation'                => array(
+							'type'        => 'select',
+							'label'       => __( 'Select An Animation', 'bb-vapor-modules-pro' ),
+							'default'     => 'slideInRight',
+							'description' => __( 'Animations only work if you have selected only one item in the carousel', 'bb-vapor-modules-pro' ),
+							'options'     => array(
+								'fadeIn'            => __( 'Fade In', 'bb-vapor-modules-pro' ),
+								'fadeInDown'        => __( 'Fade In Down', 'bb-vapor-modules-pro' ),
+								'fadeInDownBig'     => __( 'Fade In Down Big', 'bb-vapor-modules-pro' ),
+								'fadeInLeft'        => __( 'Fade In Left', 'bb-vapor-modules-pro' ),
+								'fadeInLeftBig'     => __( 'Fade In Left Big', 'bb-vapor-modules-pro' ),
+								'fadeInRight'       => __( 'Fade In Right', 'bb-vapor-modules-pro' ),
+								'fadeInRightBig'    => __( 'Fade In Right Big', 'bb-vapor-modules-pro' ),
+								'fadeInUp'          => __( 'Fade In Up', 'bb-vapor-modules-pro' ),
+								'fadeInRightUp'     => __( 'Fade In Up Big', 'bb-vapor-modules-pro' ),
+								'bounceIn'          => __( 'Bounce In', 'bb-vapor-modules-pro' ),
+								'bounceInDown'      => __( 'Bounce In Down', 'bb-vapor-modules-pro' ),
+								'bounceInLeft'      => __( 'Bounce In Left', 'bb-vapor-modules-pro' ),
+								'bounceInRight'     => __( 'Bounce In Right', 'bb-vapor-modules-pro' ),
+								'bounceInUp'        => __( 'Bounce In Up', 'bb-vapor-modules-pro' ),
+								'flip'              => __( 'Flip', 'bb-vapor-modules-pro' ),
+								'flipInX'           => __( 'Flip In X', 'bb-vapor-modules-pro' ),
+								'flipInY'           => __( 'Flip In Y', 'bb-vapor-modules-pro' ),
+								'lightSpeedIn'      => __( 'Light Speed In', 'bb-vapor-modules-pro' ),
+								'rotateIn'          => __( 'Rotate In', 'bb-vapor-modules-pro' ),
+								'rotateInDownLeft'  => __( 'Rotate In Down Left', 'bb-vapor-modules-pro' ),
+								'rotateInDownRight' => __( 'Rotate In Down Right', 'bb-vapor-modules-pro' ),
+								'rotateInUpLeft'    => __( 'Rotate In Up Left', 'bb-vapor-modules-pro' ),
+								'rotateInUpRight'   => __( 'Rotate In Up Right', 'bb-vapor-modules-pro' ),
+								'slideInUp'         => __( 'Slide In Up', 'bb-vapor-modules-pro' ),
+								'slideInDown'       => __( 'Slide In Down', 'bb-vapor-modules-pro' ),
+								'slideInLeft'       => __( 'Slide In Left', 'bb-vapor-modules-pro' ),
+								'slideInRight'      => __( 'Slide In Right', 'bb-vapor-modules-pro' ),
+								'zoomIn'            => __( 'Zoom In', 'bb-vapor-modules-pro' ),
+								'zoomInDown'        => __( 'Zoom In Down', 'bb-vapor-modules-pro' ),
+								'zoomInLeft'        => __( 'Zoom In Left', 'bb-vapor-modules-pro' ),
+								'zoomInRight'       => __( 'Zoom In Right', 'bb-vapor-modules-pro' ),
+								'zoomInUp'          => __( 'Zoom In Up', 'bb-vapor-modules-pro' ),
+							),
+						),
+						'show_nav_buttons'         => array(
+							'type'    => 'select',
+							'label'   => __( 'Show Nav Buttons', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+							),
+							'default' => 'yes',
+							'toggle'  => array( 'yes' => array( 'fields' => array( 'nav_color', 'nav_background' ) ) ),
+						),
+						'nav_color'                => array(
+							'type'    => 'color',
+							'label'   => __( 'Navigation Arrows Color', 'bb-vapor-modules-pro' ),
+							'default' => '000000',
+						),
+						'nav_background'           => array(
 							'type'       => 'color',
-							'label'      => __( 'Card Background Color', 'bb-vapor-modules-pro' ),
-							'show_reset' => true,
+							'label'      => __( 'Navigation Arrows Background Color', 'bb-vapor-modules-pro' ),
 							'default'    => 'FFFFFF',
-						),
-						'card_text_color'               => array(
-							'type'       => 'color',
-							'label'      => __( 'Card Text Color', 'bb-vapor-modules-pro' ),
 							'show_reset' => true,
-							'default'    => '000000',
+							'show_alpha' => true,
 						),
-						'max_image_width'               => array(
-							'type'        => 'unit',
-							'label'       => __( 'Max Card Width', 'bb-vapor-modules-pro' ),
-							'responsive'  => true,
-							'default'     => '1000',
-							'description' => __( 'Can override your column width settings', 'bb-vapor-modules-pro' ),
+						'show_nav_bullets'         => array(
+							'type'    => 'select',
+							'label'   => __( 'Show Nav Bullets', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+							),
+							'default' => 'yes',
+							'toggle'  => array( 'yes' => array( 'fields' => array( 'nav_bullets_color', 'nav_bullets_active_color' ) ) ),
+						),
+						'nav_bullets_color'        => array(
+							'type'    => 'color',
+							'label'   => __( 'Navigation Bullets Color', 'bb-vapor-modules-pro' ),
+							'default' => 'D6D6D6',
+						),
+						'nav_bullets_active_color' => array(
+							'type'    => 'color',
+							'label'   => __( 'Navigation Bullets Active Color', 'bb-vapor-modules-pro' ),
+							'default' => '869791',
+						),
+						'slide_duration'           => array(
+							'type'    => 'unit',
+							'label'   => __( 'Slide Duration in Milliseconds', 'bb-vapor-modules-pro' ),
+							'default' => '5000',
+						),
+						'slide_loop'               => array(
+							'type'    => 'select',
+							'label'   => __( 'Loop', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+							),
+							'default' => 'yes',
+						),
+						'auto_play'                => array(
+							'type'    => 'select',
+							'label'   => __( 'Auto Play?', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+							),
+							'default' => 'yes',
+						),
+						'pause_hover'              => array(
+							'type'    => 'select',
+							'label'   => __( 'Pause on Hover?', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+							),
+							'default' => 'yes',
+						),
+						'auto_height'              => array(
+							'type'    => 'select',
+							'label'   => __( 'Auto Height?', 'bb-vapor-modules-pro' ),
+							'options' => array(
+								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
+								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
+							),
+							'default' => 'yes',
 						),
 					),
 				),
 			),
 		),
-		'typography' => array(
-			'title'    => __( 'Typography', 'bb-vapor-modules-pro' ),
-			'sections' => array(
-				'typography' => array(
-					'title'  => __( 'Typography Settings', 'bb-vapor-modules-pro' ),
-					'fields' => array(
-						'likes_comments_typography' => array(
-							'type'       => 'typography',
-							'label'      => __( 'Likes and Comments Typography', 'bb-vapor-modules-pro' ),
-							'responsive' => true,
-						),
-						'caption_typography'        => array(
-							'type'       => 'typography',
-							'label'      => __( 'Caption Typography', 'bb-vapor-modules-pro' ),
-							'responsive' => true,
-						),
-					),
-				),
-			),
-		),
-		'buttons'    => array(
+		'buttons' => array(
 			'title'    => __( 'Buttons', 'bb-vapor-modules-pro' ),
 			'sections' => array(
 				'buttons' => array(
 					'title'  => __( 'Button Settings', 'bb-vapor-modules-pro' ),
 					'fields' => array(
-						'show_load_more_button'      => array(
-							'type'        => 'select',
-							'label'       => __( 'Show Load More Button', 'bb-vapor-modules-pro' ),
-							'options'     => array(
-								'yes' => __( 'Yes', 'bb-vapor-modules-pro' ),
-								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
-							),
-							'default'     => 'yes',
-							'description' => __( 'Showing Load More is not recommended for the Masonry layout', 'bb-vapor-modules-pro' ),
-						),
 						'show_follow_us_button'      => array(
 							'type'    => 'select',
 							'label'   => __( 'Show Follow Us Button', 'bb-vapor-modules-pro' ),
@@ -209,23 +230,6 @@ FLBuilder::register_module(
 								'no'  => __( 'No', 'bb-vapor-modules-pro' ),
 							),
 							'default' => 'yes',
-						),
-						'load_more_background_color' => array(
-							'type'       => 'color',
-							'label'      => __( 'Show More Background Color', 'bb-vapor-modules-pro' ),
-							'show_reset' => true,
-							'default'    => '000000',
-						),
-						'load_more_text_color'       => array(
-							'type'       => 'color',
-							'label'      => __( 'Show More Text Color', 'bb-vapor-modules-pro' ),
-							'show_reset' => true,
-							'default'    => 'FFFFFF',
-						),
-						'load_more_text'             => array(
-							'type'    => 'text',
-							'label'   => __( 'Load More Text', 'bb-vapor-modules-pro' ),
-							'default' => __( 'Load More...', 'bb-vapor-modules-pro' ),
 						),
 						'follow_background_color'    => array(
 							'type'       => 'color',
--- bbvm-modules/instagram-slideshow/includes/frontend.css.php
+++ bbvm-modules/instagram-slideshow/includes/frontend.css.php
@@ -1,9 +1,187 @@
 <?php
 /**
- * Instagram Module.
+ * Instagram Slideshow module.
  *
  * @link https://bbvapormodules.com
  *
  * @package BB Vapor Modules
- * @since 1.3.0
+ * @since 1.6.5
  */
+
+?>
+.bbvm-mfp-fade.mfp-bg {
+	opacity: 0;
+
+	-webkit-transition: all 0.50s ease-out;
+	-moz-transition: all 0.50s ease-out;
+	transition: all 0.50s ease-out;
+}
+.bbvm-mfp-fade.mfp-bg.mfp-ready {
+	opacity: 0.8;
+}
+.bbvm-mfp-fade.mfp-bg.mfp-removing {
+	opacity: 0;
+}
+.bbvm-mfp-fade.mfp-wrap .mfp-content {
+	opacity: 0;
+	-webkit-transition: all 0.50s ease-out;
+	-moz-transition: all 0.50s ease-out;
+	transition: all 0.50s ease-out;
+}
+.bbvm-mfp-fade.mfp-wrap.mfp-ready .mfp-content {
+	opacity: 1;
+}
+.bbvm-mfp-fade.mfp-wrap.mfp-removing .mfp-content {
+	opacity: 0;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .fl-node-instafeed-slideshow img {
+	max-width: <?php echo absint( $settings->image_width ); ?>px;
+	margin: 0 auto;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .fl-node-instafeed-slideshow .instagram-image-slide {
+	width: 100%;
+	text-align: center;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .bbvm-instagram-slideshow .instagram-author {
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	padding: 10px 0;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .bbvm-instagram-slideshow .instagram-author img {
+	width: 50px;
+	height: 50px;
+	border-radius: 100%;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .bbvm-instagram-slideshow .instagram-author a,
+.fl-node-<?php echo esc_html( $id ); ?> .bbvm-instagram-slideshow .instagram-author a:hover,
+.fl-node-<?php echo esc_html( $id ); ?> .bbvm-instagram-slideshow .instagram-author a:visited {
+	color: #000000;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .bbvm-instagram-slideshow .instagram-buttons {
+	margin-top: 20px;
+	text-align: center;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .bbvm-instagram-slideshow .instagram-buttons a {
+	display: inline-block;
+	padding: 10px 20px;
+	text-decoration: none;
+	border-radius: 5px;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .bbvm-instagram-slideshow .instagram-buttons a.instagram-follow,
+.fl-node-<?php echo esc_html( $id ); ?> .bbvm-instagram-slideshow .instagram-buttons a.instagram-follow:hover,
+.fl-node-<?php echo esc_html( $id ); ?> .bbvm-instagram-slideshow .instagram-buttons a.instagram-follow:visited {
+	color: <?php echo esc_html( BBVapor_Modules_Pro::get_color( $settings->follow_text_color ) ); ?>;
+	background: <?php echo esc_html( BBVapor_Modules_Pro::get_color( $settings->follow_background_color ) ); ?>;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .owl-dots .owl-dot span {
+	background: #<?php echo esc_html( $settings->nav_bullets_color ); ?>;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .owl-dots .owl-dot.active span {
+	background: #<?php echo esc_html( $settings->nav_bullets_active_color ); ?>;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .owl-nav {
+	position: absolute;
+	top: calc(50% - 44px);
+	width: 100%;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .owl-prev {
+	position: absolute;
+	left: 0;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .owl-prev,
+.fl-node-<?php echo esc_html( $id ); ?> .owl-next {
+	float: left;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .owl-next  {
+	float: right;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .owl-prev i,
+.fl-node-<?php echo esc_html( $id ); ?> .owl-next i{
+	font-size: 24px;
+	color: #<?php echo esc_html( $settings->nav_color ); ?>;
+	background: <?php echo esc_html( BBVapor_Modules_Pro::get_color( $settings->nav_background ) ); ?>;
+	padding: 10px;
+	border-radius: 50%;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .owl-theme .owl-nav [class*=owl-]:hover {
+	background: transparent;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .instagram-author {
+	display: flex;
+	justify-content: flex-start;
+	align-items: center;
+	padding: 10px 0;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .instagram-author img {
+	width: 50px;
+	height: 50px;
+	border-radius: 100%;
+}
+.fl-node-<?php echo esc_html( $id ); ?> .instagram-author a,
+.fl-node-<?php echo esc_html( $id ); ?> .instagram-author a:hover,
+.fl-node-<?php echo esc_html( $id ); ?> .instagram-author a:visited {
+	color: #000000;
+}
+<?php
+FLBuilderCSS::rule(
+	array(
+		'selector' => ".fl-node-$id .instagram-image-slide",
+		'media'    => 'default', // Optional. Can be `default`, `medium`, `responsive` or a custom statement.
+		'props'    => array(
+			'width' => $settings->card_width . '%',
+		),
+	)
+);
+FLBuilderCSS::rule(
+	array(
+		'selector' => ".fl-node-$id .instagram-image-slide",
+		'media'    => 'medium', // Optional. Can be `default`, `medium`, `responsive` or a custom statement.
+		'props'    => array(
+			'width' => $settings->card_width_medium . '%',
+		),
+	)
+);
+FLBuilderCSS::rule(
+	array(
+		'selector' => ".fl-node-$id .instagram-image-slide",
+		'media'    => 'responsive', // Optional. Can be `default`, `medium`, `responsive` or a custom statement.
+		'props'    => array(
+			'width' => $settings->card_width_responsive . '%',
+		),
+	)
+);
+FLBuilderCSS::dimension_field_rule(
+	array(
+		'settings'     => $settings,
+		'setting_name' => 'card_padding',
+		'selector'     => ".fl-node-$id .instagram-image-slide",
+		'unit'         => 'px',
+		'props'        => array(
+			'padding-top'    => 'card_padding_top',
+			'padding-right'  => 'card_padding_right',
+			'padding-bottom' => 'card_padding_bottom',
+			'padding-left'   => 'card_padding_left',
+		),
+	)
+);
+FLBuilderCSS::dimension_field_rule(
+	array(
+		'settings'     => $settings,
+		'setting_name' => 'card_margin',
+		'selector'     => ".fl-node-$id .instagram-image-slide",
+		'unit'         => 'px',
+		'props'        => array(
+			'margin-top'    => 'card_margin_top',
+			'margin-right'  => 'card_margin_right',
+			'margin-bottom' => 'card_margin_bottom',
+			'margin-left'   => 'card_margin_left',
+		),
+	)
+);
+?>
+.fl-node-<?php echo esc_html( $id ); ?> .is-placeholder {
+	background: transparent !important;
+	padding: 0 !important;
+	margin: 0 !important;
+}
